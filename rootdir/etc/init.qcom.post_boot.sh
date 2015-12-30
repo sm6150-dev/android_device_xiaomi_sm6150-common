0632b91ac923ab4610a074417bf6d6f2ce1cb000
Rajulu Ponnada <rajulu@codeaurora.org>
2015-12-30 17:58:57 +0530

    titanium: Governor,sched tuning parameters
    Updated governor, sched, cpubw tuning parameters for msmtitanium
    
    Change-Id: I32e2eab562c95b82d805e32dee35b3d27f767006

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1046,7 +1046,7 @@ case "$target" in
 esac
 
 case "$target" in
-    "msm8953")
+    "msmtitanium")
 
         if [ -f /sys/devices/soc0/soc_id ]; then
             soc_id=`cat /sys/devices/soc0/soc_id`
@@ -1055,10 +1055,128 @@ case "$target" in
         fi
 
         case "$soc_id" in
-            "293")
+            "293" | "304" )
+                #scheduler settings
+                echo 3 > /proc/sys/kernel/sched_window_stats_policy
+                echo 3 > /proc/sys/kernel/sched_ravg_hist_size
 
-                # Keep low power modes Disabled
-                echo 1 > /sys/module/lpm_levels/parameters/sleep_disabled
+                #task packing settings
+                echo 1 > /sys/devices/system/cpu/cpu0/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu1/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu2/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu3/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu4/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu5/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu6/sched_static_cpu_pwr_cost
+                echo 1 > /sys/devices/system/cpu/cpu7/sched_static_cpu_pwr_cost
+
+                #init task load, restrict wakeups to preferred cluster
+                echo 15 > /proc/sys/kernel/sched_init_task_load
+                # spill load is set to 100% by default in the kernel
+                echo 3 > /proc/sys/kernel/sched_spill_nr_run
+                # Apply inter-cluster load balancer restrictions
+                echo 1 > /proc/sys/kernel/sched_restrict_cluster_spill
+
+
+                for devfreq_gov in /sys/class/devfreq/qcom,mincpubw*/governor
+                do
+                    echo "cpufreq" > $devfreq_gov
+                done
+
+                for devfreq_gov in /sys/class/devfreq/soc:qcom,cpubw/governor
+                do
+                    echo "bw_hwmon" > $devfreq_gov
+                    for cpu_io_percent in /sys/class/devfreq/soc:qcom,cpubw/bw_hwmon/io_percent
+                    do
+                        echo 20 > $cpu_io_percent
+                    done
+                for cpu_guard_band in /sys/class/devfreq/soc:qcom,cpubw/bw_hwmon/guard_band_mbps
+                    do
+                        echo 30 > $cpu_guard_band
+                    done
+                done
+
+                for gpu_bimc_io_percent in /sys/class/devfreq/soc:qcom,gpubw/bw_hwmon/io_percent
+                do
+                    echo 40 > $gpu_bimc_io_percent
+                done
+                # disable thermal & BCL core_control to update interactive gov settings
+                echo 0 > /sys/module/msm_thermal/core_control/enabled
+                for mode in /sys/devices/soc.0/qcom,bcl.*/mode
+                do
+                    echo -n disable > $mode
+                done
+                for hotplug_mask in /sys/devices/soc.0/qcom,bcl.*/hotplug_mask
+                do
+                    bcl_hotplug_mask=`cat $hotplug_mask`
+                    echo 0 > $hotplug_mask
+                done
+                for hotplug_soc_mask in /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask
+                do
+                    bcl_soc_hotplug_mask=`cat $hotplug_soc_mask`
+                    echo 0 > $hotplug_soc_mask
+                done
+                for mode in /sys/devices/soc.0/qcom,bcl.*/mode
+                do
+                    echo -n enable > $mode
+                done
+
+                #governor settings
+                echo 1 > /sys/devices/system/cpu/cpu0/online
+                echo "interactive" > /sys/devices/system/cpu/cpufreq/scaling_governor
+                echo "19000 1401600:39000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                echo 85 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+                echo 1401600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+                echo "85 1401600:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+                echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
+                echo 652800 > /sys/devices/system/cpu/cpufreq/scaling_min_freq
+
+                # re-enable thermal & BCL core_control now
+                echo 1 > /sys/module/msm_thermal/core_control/enabled
+                for mode in /sys/devices/soc.0/qcom,bcl.*/mode
+                do
+                    echo -n disable > $mode
+                done
+                for hotplug_mask in /sys/devices/soc.0/qcom,bcl.*/hotplug_mask
+                do
+                    echo $bcl_hotplug_mask > $hotplug_mask
+                done
+                for hotplug_soc_mask in /sys/devices/soc.0/qcom,bcl.*/hotplug_soc_mask
+                do
+                    echo $bcl_soc_hotplug_mask > $hotplug_soc_mask
+                done
+                for mode in /sys/devices/soc.0/qcom,bcl.*/mode
+                do
+                    echo -n enable > $mode
+                done
+
+                # Bring up all cores online
+                echo 1 > /sys/devices/system/cpu/cpu1/online
+                echo 1 > /sys/devices/system/cpu/cpu2/online
+                echo 1 > /sys/devices/system/cpu/cpu3/online
+                echo 1 > /sys/devices/system/cpu/cpu4/online
+                echo 1 > /sys/devices/system/cpu/cpu5/online
+                echo 1 > /sys/devices/system/cpu/cpu6/online
+                echo 1 > /sys/devices/system/cpu/cpu7/online
+
+                # Enable low power modes
+                echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+
+                # SMP scheduler
+                echo 100 > /proc/sys/kernel/sched_upmigrate
+                echo 100 > /proc/sys/kernel/sched_downmigrate
+
+                # Enable sched guided freq control
+                echo 1 > /sys/devices/system/cpu/cpufreq/interactive/use_sched_load
+                echo 1 > /sys/devices/system/cpu/cpufreq/interactive/use_migration_notif
+                echo 200000 > /proc/sys/kernel/sched_freq_inc_notify
+                echo 200000 > /proc/sys/kernel/sched_freq_dec_notify
+
+                # Set Memory parameters
+                configure_memory_parameters
 	;;
 	esac
 	;;
@@ -1769,7 +1887,7 @@ case "$target" in
         #start perfd after setprop
         start perfd # start perfd on 8916 and 8939
     ;;
-    "msm8937")
+    "msm8937" | "msmtitanium")
         rm /data/system/perfd/default_values
         start perfd
     ;;

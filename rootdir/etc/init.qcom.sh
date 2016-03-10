29dfaa3ad6860a5d1f9f8d1075fe9cd7b8823165
Runmin Wang <runminw@codeaurora.org>
2016-03-10 15:36:06 -0800

    init: qcom: start irq balancer for msmcobalt
    Start irq balancer for msmcobalt.
    
    CRs-Fixed: 989171
    Change-Id: I292d9131a98fb82166f4e4f5ce304b84f2c7a0d7

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -219,7 +219,7 @@ case "$target" in
                   ;;
         esac
         ;;
-    "msm8994" | "msm8992")
+    "msm8994" | "msm8992" | "msmcobalt")
         start_msm_irqbalance
         ;;
     "msm8996")

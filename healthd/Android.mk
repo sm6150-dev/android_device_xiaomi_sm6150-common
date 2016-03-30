0a32f71b7b1971ada38f142409fd0c011e053467
Anshul Verma <vanshu@codeaurora.org>
2016-03-30 16:05:27 +0530

    Initial release bring-up
    Changes to bring-up tree on new release. Some permanent
    and some temporary until all modules are in place.
    
    Change-Id: I72ac646012d03f9442313d08a7879a55e789d1d3

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -4,5 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp healthd_msm_alarm.cpp
 LOCAL_MODULE := libhealthd.msm
 LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
+LOCAL_C_INCLUDES := system/core/healthd system/core/healthd/include/healthd
+#LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
 include $(BUILD_STATIC_LIBRARY)

7856219225425b4d356b820a019c8d6d35a14365
feifanz <feifanz@codeaurora.org>
2016-04-01 09:54:23 +0800

    init: early_boot: Add display DFS node permission
    Add permisson for display DFS nodes access, it's a new feature which allow
    HDMI device tuning FPS during video playback.
    
    CRs-fixed: 1032036
    Change-Id: Ia17995d00fd763a147192a27170b97ebb23e4786

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -256,6 +256,8 @@ function setHDMIPermission() {
    set_perms $file/video_mode system.graphics 0664
    set_perms $file/format_3d system.graphics 0664
    set_perms $file/s3d_mode system.graphics 0664
+   set_perms $file/dynamic_fps system.graphics 0664
+   set_perms $file/msm_fb_dfps_mode system.graphics 0664
    set_perms $file/cec/enable system.graphics 0664
    set_perms $file/cec/logical_addr system.graphics 0664
    set_perms $file/cec/rd_msg system.graphics 0664

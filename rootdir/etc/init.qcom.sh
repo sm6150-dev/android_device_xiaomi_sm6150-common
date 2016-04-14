2fa38a51d394efe58985923023cfef747136b9ac
Richard LIU <richardl@codeaurora.org>
2016-04-14 17:12:44 +0800

    copy MBN list txt file to data partition
    copy MBN list txt file to data partition
    
    Change-Id: I06468a412cd1015776ded24947dfd6e64718640b

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -342,4 +342,6 @@ if [ ! -f /firmware/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_versio
     cp /firmware/verinfo/ver_info.txt /data/misc/radio/ver_info.txt
     chown radio.radio /data/misc/radio/ver_info.txt
 fi
+cp /firmware/image/modem_pr/mbn_ota.txt /data/misc/radio/modem_config
+chown radio.radio /data/misc/radio/modem_config/mbn_ota.txt
 echo 1 > /data/misc/radio/copy_complete

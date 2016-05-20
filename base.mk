fc58e62ab5142975b6b2f9b2a0984f4b4150290f
Mengjun Leng <mengju@codeaurora.org>
2016-05-20 10:28:49 +0800

    common: remove some unused library source for qrdplus structure adjustment
    Change-Id: Iba9048f89886f595a674d2518ce6d4353dbb40f5
    CRs-Fixed: 1020148

--- base.mk
+++ base.mk
@@ -563,12 +563,9 @@ MM_VIDEO += ExoplayerDemo
 
 #OPENCORE
 OPENCORE := libomx_aacdec_sharedlibrary
-OPENCORE += libomx_amrdec_sharedlibrary
-OPENCORE += libomx_amrenc_sharedlibrary
 OPENCORE += libomx_avcdec_sharedlibrary
 OPENCORE += libomx_m4vdec_sharedlibrary
 OPENCORE += libomx_mp3dec_sharedlibrary
-OPENCORE += libomx_sharedlibrary
 OPENCORE += libopencore_author
 OPENCORE += libopencore_common
 OPENCORE += libopencore_download
@@ -579,11 +576,8 @@ OPENCORE += libopencore_net_support
 OPENCORE += libopencore_player
 OPENCORE += libopencore_rtsp
 OPENCORE += libopencore_rtspreg
-OPENCORE += libpvdecoder_gsmamr
 OPENCORE += libpvplayer_engine
-OPENCORE += libpvamrwbdecoder
 OPENCORE += libpvauthorengine
-OPENCORE += libomx_amr_component_lib
 OPENCORE += pvplayer
 OPENCORE += pvplayer_engine_test
 

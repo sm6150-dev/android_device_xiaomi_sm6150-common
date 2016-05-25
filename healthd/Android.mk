383b5447fd24c30ad2d8c0644d2d7aff3d45b504
Ashay Jaiswal <ashayj@codeaurora.org>
2016-05-25 10:34:27 +0530

    healthd: update header file path
    healthd daemon uses minui header files add path of minui
    header files in local include.
    
    Change-Id: I124cede0b5d81c9e48fb463468435d010af27bd3

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -4,6 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp healthd_msm_alarm.cpp
 LOCAL_MODULE := libhealthd.msm
 LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := system/core/healthd system/core/healthd/include/healthd
-#LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
+LOCAL_C_INCLUDES := system/core/healthd/include/healthd/ bootable/recovery
 include $(BUILD_STATIC_LIBRARY)

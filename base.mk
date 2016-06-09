78fdf03f42538f220d5cdf49bf78fcfa05f83834
Gaurav Singhal <gsinghal@codeaurora.org>
2016-06-09 20:13:41 +0530

    NFC: common: Add feature indication for HCE on NFC-F
    The host-based card emulation on NFC-F has its own
    feature indication file.
    
    Added to build targets.
    
    Change-Id: I608ec7eae65fe1cf11e303b9910fb374d50366c8

--- base.mk
+++ base.mk
@@ -909,7 +909,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:system/etc/permissions/android.hardware.nfc.hcef.xml
 endif
 
 # enable overlays to use our version of

d92254098425f7443998216af3580c9418c6738e
Ananta Kishore K <akollipa@codeaurora.org>
2016-08-10 15:54:52 +0530

    Graphics: Disable Vulkan for 8917
    Vulkan is not supported on 8917.
    Set "persist.graphics.vulkan.disable" to true.
    Framework checks for this flag and disables
    Vulkan features for 8917.
    
    Change-Id: I6ee6b136bfc153cc6dd26d5e4b80726cdbbcfd0d

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -212,6 +212,11 @@ case "$target" in
             294|295|296|297|298|313)
                 setprop ro.opengles.version 196609
                 ;;
+            303|307|308|309)
+                # Vulkan is not supported for 8917 variants
+                setprop ro.opengles.version 196608
+                setprop persist.graphics.vulkan.disable true
+                ;;
             *)
                 setprop ro.opengles.version 196608
                 ;;

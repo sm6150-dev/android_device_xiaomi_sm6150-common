720ef934e6ae337418971ef00ed5e5bc4f832722
Ameya Thakur <ameyat@codeaurora.org>
2016-09-26 12:07:36 -0700

    common: Generate non-hlos image mount points for A/B builds
    On builds where A/B support is enabled the system image acts as the
    rootfs. In this case we can no longer create the non-hlos image mount
    points at run time as we used to. We now create them as part of the
    build itself.
    
    Change-Id: I30d98f7c50fca3ce7117b9b8d87150655bb5b440

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -21,6 +21,37 @@ recovery_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.img
 INSTALLED_USBIMAGE_TARGET := $(PRODUCT_OUT)/usbdisk.img
 endif
 
+#For A/B builds we need to create the mount points at compile time
+ifeq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE), true)
+FIRMWARE_MOUNT_POINT := $(TARGET_ROOT_OUT)/firmware
+BT_FIRMWARE_MOUNT_POINT := $(TARGET_ROOT_OUT)/bt_firmware
+DSP_MOUNT_POINT := $(TARGET_ROOT_OUT)/dsp
+PERSIST_MOUNT_POINT := $(TARGET_ROOT_OUT)/persist
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) \
+				 $(BT_FIRMWARE_MOUNT_POINT) \
+				 $(DSP_MOUNT_POINT) \
+				 $(PERSIST_MOUNT_POINT)
+$(FIRMWARE_MOUNT_POINT):
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_ROOT_OUT)/firmware
+	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/firmware
+
+$(BT_FIRMWARE_MOUNT_POINT):
+	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_ROOT_OUT)/bt_firmware
+	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/bt_firmware
+
+$(DSP_MOUNT_POINT):
+	@echo "Creating $(DSP_MOUNT_POINT)"
+	@mkdir -p $(TARGET_ROOT_OUT)/dsp
+	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/dsp
+
+$(PERSIST_MOUNT_POINT):
+	@echo "Creating $(PERSIST_MOUNT_POINT)"
+	@mkdir -p $(TARGET_ROOT_OUT)/persist
+	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/persist
+endif
+
 #----------------------------------------------------------------------
 # Generate secure boot image
 #----------------------------------------------------------------------

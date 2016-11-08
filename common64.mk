11925aff6e30c6756e476614f9d2cb4ccd1b36ba
Qiang Chen <cqiang@codeaurora.org>
2016-11-08 10:29:56 +0800

    Set default multisim property to dsds by default.
    CRs-Fixed: 1087695
    
    Change-Id: I8021d35550b2a9e1ebf5aa5c44f9ab540e5b10ed

--- common64.mk
+++ common64.mk
@@ -16,7 +16,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.sib16_support=1 \
-    persist.radio.custom_ecc=1
+    persist.radio.custom_ecc=1 \
+    persist.radio.multisim.config=dsds
 
 ifneq ($(BOARD_FRP_PARTITION_NAME),)
     PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/$(BOARD_FRP_PARTITION_NAME)

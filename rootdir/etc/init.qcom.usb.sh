f6de9aae3cc963cb04c36a7fe492eefaa51ea1ef
Jack Pham <jackp@codeaurora.org>
2017-01-23 17:01:16 -0800

    init.qcom.usb: Move setting of ConfigFS serialnumber to .sh
    In some cases the 'ro.serialno' may not be populated by the
    bootloader. Since init.rc syntax does not support conditional
    statements, add checking of existence of this property to the
    init.qcom.usb.sh script so that a dummy value can be assigned
    and written to ConfigFS in case it is not defined.
    
    Change-Id: I438316691c608056ef870e974045b547ef654409

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -195,11 +195,20 @@ esac
 
 # check configfs is mounted or not
 if [ -d /config/usb_gadget ]; then
+	# Chip-serial is used for unique MSM identification in Product string
 	msm_serial=`cat /sys/devices/soc0/serial_number`;
 	msm_serial_hex=`printf %08X $msm_serial`
 	machine_type=`cat /sys/devices/soc0/machine`
 	product_string="$machine_type-$soc_hwplatform _SN:$msm_serial_hex"
 	echo "$product_string" > /config/usb_gadget/g1/strings/0x409/product
+
+	# ADB requires valid iSerialNumber; if ro.serialno is missing, use dummy
+	serialno=`getprop ro.serialno`
+	if [ "$serialno" == "" ]; then
+	    serialno=1234567
+	fi
+	echo $serialno > /config/usb_gadget/g1/strings/0x409/serialnumber
+
 	setprop sys.usb.configfs 1
 fi
 

c0a3cc837e997dd05afd6a8f6e1f311ca0179e71
Mayank Rana <mrana@codeaurora.org>
2017-04-14 19:19:00 -0700

    init.qcom.usb.sh: update sys.usb.config using persist.sys.usb.config
    Currently the 'qcom-usb-sh' service which executes init.qcom.usb.sh
    is running after "on boot" trigger based execution completed. When
    init.qcom.usb.sh is started running, updating sys.usb.config from
    $persist.sys.usb.config is already performed from init.usb.rc. This
    results into required on "property:persist.sys.usb.config" rule being
    not executing causing no USB enumeration. Also adb only related rule
    doesn't execute in some cases as other property like sys.usb.controller,
    sys.usb.configfs is not updated due delayed init.qcom.usb.sh execution.
    Fix this issue by checking sys.usb.config and persist.sys.usb.config
    value within init.qcom.usb.sh, and update sys.usb.config value using
    persist.sys.usb.config based value if it is not same.
    
    Change-Id: I88ef6cad63e447598613eaefdf18c00768e39d49

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -223,6 +223,15 @@ if [ -d /config/usb_gadget ]; then
 	fi
 	echo $serialno > /config/usb_gadget/g1/strings/0x409/serialnumber
 
+	persist_comp=`getprop persist.sys.usb.config`
+	comp=`getprop sys.usb.config`
+	echo $persist_comp
+	echo $comp
+	if [ "$comp" != "$persist_comp" ]; then
+		echo "setting sys.usb.config"
+		setprop sys.usb.config $persist_comp
+	fi
+
 	setprop sys.usb.configfs 1
 fi
 

c33f452c93bd1f01935ab81635167e6df85fd7bf
Mayank Rana <mrana@codeaurora.org>
2017-05-02 17:33:47 -0700

    init.qcom.usb.sh: Set 9091 as USB default composition on SDM845
    This change sets USB default composition as 9091: DIAG, DUN, RMNET,
    ADB interfaces on SDM845.
    
    Change-Id: I1c9c5fe78358b7618d3b900a280f90b300ba7f57

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -162,7 +162,7 @@ case "$usb_config" in
 	              "msm8952" | "msm8953")
 		          setprop persist.sys.usb.config diag,serial_smd,rmnet_ipa,adb
 		      ;;
-	              "msm8998" | "sdm660")
+	              "msm8998" | "sdm660" | "sdm845")
 		          setprop persist.sys.usb.config diag,serial_cdev,rmnet,adb
 		      ;;
 	              *)

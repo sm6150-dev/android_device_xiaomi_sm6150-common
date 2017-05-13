d4b5cf42ac66478a1b7596ac05e1b8260aede4ff
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-13 13:54:36 -0700

    Install init.qcom.debug.sh to /vendor.
    Change-Id: I1543d4b21c82069ebd24a8211a28a4bf2dc2767c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -184,6 +184,7 @@ LOCAL_MODULE       := init.qcom.debug.sh
 LOCAL_MODULE_TAGS  := debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.debug.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

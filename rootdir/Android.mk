dca0ac4cba43e16f6c3cd6c693205ad0aa26ccb8
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-13 13:48:00 -0700

    Install init.qcom.syspart_fixup.sh to /vendor.
    Change-Id: I448dfd5d34f28e22f18d5d73578cc168f3097194

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -152,7 +152,7 @@ LOCAL_MODULE       := init.qcom.syspart_fixup.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.syspart_fixup.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

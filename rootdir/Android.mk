44899fdb0953d163f381041f45d30fd8fc30cc79
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-13 13:45:40 -0700

    Install and launch init.class_main.sh from /vendor.
    Change-Id: I1c9bca8e4274951954262ad65f1871da0b7e7db6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -106,7 +106,7 @@ LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.class_main.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

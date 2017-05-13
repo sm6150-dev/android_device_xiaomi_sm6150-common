8190d767f9491bedf3166d6348747a6b4674a14c
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-13 13:25:04 -0700

    Install and launch init.qcom.post_boot.sh from /vendor.
    Change-Id: Idf78667baf2c439c7e97bbf1a9028030c1bcc72f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,6 +16,7 @@ LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

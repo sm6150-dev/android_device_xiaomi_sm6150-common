311741b2468f7e58becf3021496f2d8418e387eb
AnilKumar Chimata <anilc@codeaurora.org>
2017-05-25 13:17:05 +0530

    cryptfs_hw: Add liblog for logging
    Add liblog to cryptfs_hw to compile the adb log message related
    functions to enable logging.
    
    Change-Id: Ibc6b8617431cae687d964d1ab56ac25e1b1b96f2

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -9,7 +9,9 @@ commonSharedLibraries := \
                         libcutils \
                         libutils \
                         libdl \
-                        libhardware
+                        libhardware \
+                        liblog
+
 commonIncludes := \
                   hardware/libhardware/include/hardware/
 

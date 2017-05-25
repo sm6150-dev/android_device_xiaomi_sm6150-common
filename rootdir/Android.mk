66220ef805ed1264f8291c618bc0681794fb520d
Srinivas Dasari <dasaris@codeaurora.org>
2017-05-25 14:31:36 +0530

    Install init.crda.sh to /vendor
    Change-Id: Iab3687e1c7219e0666254511194c4f0a1f1e078d
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,6 +59,7 @@ LOCAL_MODULE       := init.crda.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.crda.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

9d44b2081f0883a9f086bce09b7542c508fa4bfc
Srinivas Dasari <dasaris@codeaurora.org>
2017-05-25 14:18:49 +0530

    Install qca6234-service.sh to /vendor
    Change-Id: Idefb5b2dee699af33923d539fd93fa4a2204d06a
    Signed-off-by: Sandeep Patil <sspatil@google.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -160,6 +160,7 @@ LOCAL_MODULE       := qca6234-service.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/qca6234-service.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

66808a533802748ee3a243517b22bcd896b00b6b
Mayank Rana <mrana@codeaurora.org>
2017-05-30 15:55:13 -0700

    usb: move usb related .rc and .sh to /vendor
    This change moves init.qcom.usb.rc, init.msm.usb.configfs.rc and
    init.qcom.usb.sh to /vendor image.
    
    Change-Id: Ie63c7bb67c05f9372413549a1c007a7542fe5006

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -135,7 +135,7 @@ LOCAL_MODULE       := init.qcom.usb.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

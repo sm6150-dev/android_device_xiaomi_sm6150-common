8d231f80ee1fced0436c33392bc879c94a2e964c
Vara Prasad A V S G <vavsg@codeaurora.org>
2017-06-11 02:27:40 +0530

    Install init.qti.ims.sh to /vendor.
    Change-Id: I9e142d3a6f9eb85db632d857b60ce6707d6f63ff

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -24,6 +24,7 @@ LOCAL_MODULE       := init.qti.ims.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qti.ims.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

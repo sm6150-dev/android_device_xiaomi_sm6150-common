985b74ead481b6205174f00424be2c9bce85c5f8
Mao Jinlong <c_jmao@codeaurora.org>
2017-06-12 16:09:07 +0800

    healthd : Changes of power off charging alarm
    1. Start alarm thread when device enters power off charging mode.
    Reboot the device when alarm time is up during power off charging.
    
    2. Add reboot function to event data.If the alarm event happens, call
    reboot function immediately to ensure that device won't go to suspend
    during calling reboot function.
    
    3. Add alarm seconds check in case of that power off alarm time is up
    when start alarm thread. If the final alarm time is less than the
    estimated time of next boot completed to fire, that means it is not able
    to fire the last power off alarm at the right time, so just miss it.
    
    CRs-Fixed: 2054302
    Change-Id: I40bd9c35a9e42aeabb742603e161c2049cd0b5f5

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,16 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := healthd_board_msm.cpp
+LOCAL_SRC_FILES := healthd_board_msm.cpp healthd_msm_alarm.cpp
 LOCAL_MODULE := libhealthd.msm
+
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := \
     system/core/healthd/include/healthd/ \
     system/core/base/include \
     bootable/recovery \
     bootable/recovery/minui/include
-ifneq ($(TARGET_USES_AOSP),true)
-LOCAL_SRC_FILES += healthd_msm_alarm.cpp
-LOCAL_CFLAGS    += -DQTI_BSP=1
-endif
+
 include $(BUILD_STATIC_LIBRARY)

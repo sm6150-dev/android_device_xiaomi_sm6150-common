1ebd9fbd551744f9951cdd489477b0235a6f56ba
Niranjan Dumbala <dnreddy@qti.qualcomm.com>
2017-06-18 05:25:42 -0700

    Revert "Move init.qcom.coex.sh to /vendor"
    This reverts commit 11e04da10e78c41732e424027859f46e3109111b.
    
    Change-Id: I6537206314bc9bf74628ae48ed0b6a2eef1acfca

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -37,7 +37,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.coex.sh
-LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.coex.sh

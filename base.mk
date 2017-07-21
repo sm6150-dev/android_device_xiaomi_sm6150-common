395fe0e7475c6f8af162ee98d3b49fa9485a9b6f
Uma Mehta <umamehta@codeaurora.org>
2017-07-21 16:20:34 +0530

    media_profiles: Copy vanilla media_profile to vendor/etc
    Copy the vanilla version of media_profile.xml to vendor/etc
    to resolve boot up issues with pure AOSP system image.
    
    Change-Id: I9badc477d21918db4acf732a6c91a9326eac3163

--- base.mk
+++ base.mk
@@ -948,7 +948,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+    device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
+    device/qcom/common/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 ifneq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
 PRODUCT_COPY_FILES += \

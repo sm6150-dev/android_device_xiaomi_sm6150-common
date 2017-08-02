f242f3a0782243de378ce19a1f7efaa6e4ed37f1
Sashidhar Ganiga <sganig@codeaurora.org>
2017-08-02 14:27:37 +0530

    Change to read return value from new ReadArgs API
    ReadArgs now returns a boolean value instead of
    integer. Hence the condition check needs to be
    negated to keep same behaviour as N
    
    Change-Id: I53887822231075657220479f0e773797ad4c9935

--- recovery/oem-recovery/oem-updater.cpp
+++ recovery/oem-recovery/oem-updater.cpp
@@ -41,7 +41,7 @@ Value* DecryptFn(const char* name, State* state,  const std::vector<std::unique_
         return ErrorAbort(state,kArgsParsingFailure, "%s() expects 2 arg, got %zu", name, argv.size());
 
     std::vector<std::string> args;
-    if (ReadArgs(state,  argv, &args))
+    if (!ReadArgs(state,  argv, &args))
         return NULL;
 
     const std::string& src_file = args[0];
@@ -61,7 +61,7 @@ Value* BootUpdateFn(const char* name, State* state,  const std::vector<std::uniq
         return ErrorAbort(state,kArgsParsingFailure, "%s() expects 1 arg, got %zu", name, argv.size());
 
     std::vector<std::string> args;
-    if (ReadArgs(state,  argv, &args))
+    if (!ReadArgs(state,  argv, &args))
         return NULL;
 
     const std::string& stageStr = args[0];

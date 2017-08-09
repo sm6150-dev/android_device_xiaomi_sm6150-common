c40051cdc60c82309d938a304176f4dbfac30d59
David Ng <dave@codeaurora.org>
2017-08-08 17:15:24 -0700

    Add common config.fs AID/fs configuration file
    Common config.fs/$TARGET_FS_CONFIG_GEN AID/fs configuration
    file for all chipset inheriting from base.mk.
    
    Since legacy device-specific android_filesystem_config.h and
    config.fs do not co-exist, temporarily include config.fs only
    if legacy file is absent.  Once all legacy, device-specific
    files have been deleted, config.fs can be made unconditional.
    
    Change-Id: Ie100d0cd9db808b82d99be7139f0709390c7f3a2

--- base.mk
+++ base.mk
@@ -1056,3 +1056,17 @@ endif
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp \
+
+# Temporary handling
+#
+# Include config.fs get only if legacy device/qcom/<target>/android_filesystem_config.h
+# does not exist as they are mutually exclusive.  Once all target's android_filesystem_config.h
+# have been removed, TARGET_FS_CONFIG_GEN should be made unconditional.
+DEVICE_CONFIG_DIR := $(dir $(firstword $(subst ]],, $(word 2, $(subst [[, ,$(_node_import_context))))))
+ifeq ($(wildcard $(DEVICE_CONFIG_DIR)/android_filesystem_config.h),)
+  TARGET_FS_CONFIG_GEN := device/qcom/common/config.fs
+else
+  $(warning **********)
+  $(warning TODO: Need to replace legacy $(DEVICE_CONFIG_DIR)android_filesystem_config.h with config.fs)
+  $(warning **********)
+endif

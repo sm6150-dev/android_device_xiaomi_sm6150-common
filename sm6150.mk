b58e5184fb26c998d634e891071988c87650d91e
Abodunrinwa Toki <toki@google.com>
2017-08-16 21:32:06 +0100

    sm6150-common: Remove textclassifier models
    This reverts commit d8036f0b8fbe65eff967dc90f623d04350410d0b.
    
    ConfigUpdateInstallReceiver will be used to push an updatable
    model to the devices.
    
    Test: Checked that a new build does not bundle any of the bundle1
    models (i.e. en, es, de, fr) by default.
    
    Bug: 64746048
    Change-Id: I507aa8d5911da8ae27f951fe566bb86c1a10238b

--- sm6150.mk
+++ sm6150.mk
@@ -378,10 +378,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2

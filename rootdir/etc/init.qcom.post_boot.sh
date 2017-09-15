3c4dd4779eb4a36c086434499c439fd6a1e40530
Channagoud Kadabi <ckadabi@codeaurora.org>
2017-09-15 13:27:19 -0700

    common: sdm845: Limit the gold core min frequency
    Limit the gold core min frequency to 825 Mhz
    
    Change-Id: I4f39a92b0834be1cbe31e7c17cdafe6ce9e305e7

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -2414,6 +2414,8 @@ case "$target" in
 	echo 1574400 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/hispeed_freq
 	echo "0:1324800" > /sys/module/cpu_boost/parameters/input_boost_freq
 	echo 120 > /sys/module/cpu_boost/parameters/input_boost_ms
+	# Limit the min frequency to 825MHz
+	echo 825000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 
         # Enable bus-dcvs
         for cpubw in /sys/class/devfreq/*qcom,cpubw*

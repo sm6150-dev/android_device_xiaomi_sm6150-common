9b97341e02ea8189f3e5b1413de2dcf796f69369
Shivendra Kakrania <shiven@codeaurora.org>
2017-10-09 11:32:00 -0700

    Disable Video Component for msmnile
    Disabling compilation of video component for msmnile.
    
    Change-Id: Ie3265b21b38826359b02e8d7c186725d81eac29c
    CRs-Fixed: 2123407

--- base.mk
+++ base.mk
@@ -31,7 +31,7 @@ QSD8K_BOARD_PLATFORMS := qsd8k
 TARGET_USE_VENDOR_CAMERA_EXT := true
 
 #List of targets that use video hw
-MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 msm8937 msm8953 msm8998 apq8098_latv sdm660 sdm845 sdm670 msmnile qcs605
+MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 msm8937 msm8953 msm8998 apq8098_latv sdm660 sdm845 sdm670 qcs605
 
 #List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 apq8098_latv sdm670 qcs605

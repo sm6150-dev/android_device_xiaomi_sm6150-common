69d1257be5309514ee5015a52c6c19033059a15a
David Ng <dave@codeaurora.org>
2017-10-18 16:55:04 -0700

    Move recovery init rc file to device rootdir
    init.recovery.qcom.rc needs to be in device rootdir for it
    to be picked up by build into recovery rootdir - which in turn
    is read by recovery's init.rc at runtime.
    
    Change-Id: Ie00134201c8967d9cf48abc0ee690cd609c04676

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -80,7 +80,7 @@ LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

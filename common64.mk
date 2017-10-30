81c40e91b52803acf40af29709c2ce7ca73506d7
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2017-10-30 10:24:53 +0530

    Add priv-app permissions to whitelist.
    Added priv-app to whitelist so as to provide priv app permissions.
    
    CRs-Fixed: 2131551
    Change-Id: I0f2446e1946cdd7cf8b1641a46818317acfaefdd

--- common64.mk
+++ common64.mk
@@ -31,6 +31,9 @@ endif
 PRODUCT_COPY_FILES += \
     device/qcom/common/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+PRODUCT_COPY_FILES += \
+    device/qcom/common/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 
 ifneq ($(TARGET_DEFINES_DALVIK_HEAP), true)

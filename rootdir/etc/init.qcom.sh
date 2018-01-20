d763aa77d022d13e74d26a85c7358d5096edd839
Hua Liu <richardl@codeaurora.org>
2018-01-19 18:34:36 -0800

    Change scripts for copying MBNs
    1) Using "chmod -h" instead of "chmod"
    2) Using "mkdir -m" instread of 2 seperate commands
    for creating directory and change permission
    3) Using "write" command in RC file instead of "echo ...>"
    in bash
    4) Create "data/vendor/modem_config" for storing MBNs, which
    is read only
    5) Remove qcril.db copying in sh file, since this logic is
    not used anymore
    
    Change-Id: Ib9a9e0d32ae529dab3dc0c164062d2887dff4439

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -96,14 +96,6 @@ start_msm_irqbalance()
 	fi
 }
 
-start_copying_prebuilt_qcril_db()
-{
-    if [ -f /vendor/radio/qcril_database/qcril.db -a ! -f /data/vendor/radio/qcril.db ]; then
-        cp /vendor/radio/qcril_database/qcril.db /data/vendor/radio/qcril.db
-        chown -h radio.radio /data/vendor/radio/qcril.db
-    fi
-}
-
 baseband=`getprop ro.baseband`
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
 
@@ -378,33 +370,24 @@ case "$target" in
 esac
 
 #
-# Copy qcril.db if needed for RIL
-#
-start_copying_prebuilt_qcril_db
-echo 1 > /data/vendor/radio/db_check_done
-
-#
 # Make modem config folder and copy firmware config to that folder for RIL
 #
-if [ -f /data/vendor/radio/ver_info.txt ]; then
-    prev_version_info=`cat /data/vendor/radio/ver_info.txt`
+if [ -f /data/vendor/modem_config/ver_info.txt ]; then
+    prev_version_info=`cat /data/vendor/modem_config/ver_info.txt`
 else
     prev_version_info=""
 fi
 
 cur_version_info=`cat /firmware/verinfo/ver_info.txt`
 if [ ! -f /firmware/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
-    rm -rf /data/vendor/radio/modem_config
-    mkdir /data/vendor/radio/modem_config
-    chmod 770 /data/vendor/radio/modem_config
-    cp -r /firmware/image/modem_pr/mcfg/configs/* /data/vendor/radio/modem_config
-    chown -hR radio.radio /data/vendor/radio/modem_config
-    cp /firmware/verinfo/ver_info.txt /data/vendor/radio/ver_info.txt
-    chown radio.radio /data/vendor/radio/ver_info.txt
+    rm -rf /data/vendor/modem_config/*
+    # preserve the read only mode for all subdir and files
+    cp --preserve=m -dr /firmware/image/modem_pr/mcfg/configs/* /data/vendor/modem_config
+    cp --preserve=m -d /firmware/verinfo/ver_info.txt /data/vendor/modem_config/
+    cp --preserve=m -d /firmware/image/modem_pr/mbn_ota.txt /data/vendor/modem_config/
+    chown -hR radio.radio /data/vendor/modem_config/*
 fi
-cp /firmware/image/modem_pr/mbn_ota.txt /data/vendor/radio/modem_config
-chown radio.radio /data/vendor/radio/modem_config/mbn_ota.txt
-echo 1 > /data/vendor/radio/copy_complete
+setprop ro.runtime.mbn_copy_completed 1
 
 #check build variant for printk logging
 #current default minimum boot-time-default

a6aa0ec9cb17ac6c33d33b547c10a17270d4f304
Mohammed Javid <mjavid@codeaurora.org>
2018-04-27 18:54:41 +0530

    init.qcom.rc: Change qti service name to dataqti
    qti name is generic and updating it to dataqti so that no generic
    context will not match
    
    Change-Id: I186a642623f1ae46688a44f754fbe7ba421d042c

--- rootdir/etc/init.class_main.sh
+++ rootdir/etc/init.class_main.sh
@@ -132,11 +132,11 @@ case "$baseband" in
 
     case "$datamode" in
         "tethered")
-            start vendor.qti
+            start vendor.dataqti
             start port-bridge
             ;;
         "concurrent")
-            start vendor.qti
+            start vendor.dataqti
             start netmgrd
             start port-bridge
             ;;

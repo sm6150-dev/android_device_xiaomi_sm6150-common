ad7786acae22390303ed02a1f7f364913132ed13
Gopi Krishna Nedanuri <gnedanur@codeaurora.org>
2018-05-15 19:26:50 +0530

    init.qcom.debug.sh: Add OSM registers to DCC config.
    Move msmnile DCC and Trace config to different file and add OSM
    and remove few other registers from DCC config.
    
    Change-Id: I8c482dbd1f987c9ea87a99d2f4498f8563d2b8ed

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -206,6 +206,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qti.debug-msmnile.sh
+LOCAL_MODULE_TAGS  := debug
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qti.debug-msmnile.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.testscripts.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC

CodeLinaro-mirror/femto5g_platform_vendor_qcom-opensource_location

Handle updating the carrier configuration
Set GPS_LOCK for cases where the modem has
two SIMs

Change-Id: I9695e91a26ce63bf3bac75366d25b71be4f10b98
CRs-fixed: 2018029

loc_api/loc_api_v02/loc_api_v02_client.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, 2018, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -676,7 +676,12 @@ static const locClientRespIndTableStructT locClientRespIndTable[]= {
      sizeof(qmiLocGenReqStatusIndMsgT_v02) },
 
    { QMI_LOC_GET_CONSTELLATION_CONTROL_IND_V02,
-     sizeof(qmiLocGetConstellationConfigIndMsgT_v02) }
+     sizeof(qmiLocGetConstellationConfigIndMsgT_v02) },
+
+    // register master
+   { QMI_LOC_REGISTER_MASTER_CLIENT_IND_V02,
+     sizeof(qmiLocRegisterMasterClientIndMsgT_v02) }
+
 };
 
 
@@ -1088,7 +1093,8 @@ static void locClientIndCb
 
 bool locClientRegisterEventMask(
     locClientHandleType clientHandle,
-    locClientEventMaskType eventRegMask)
+    locClientEventMaskType eventRegMask,
+    bool bIsMaster)
 {
   locClientStatusEnumType status = eLOC_CLIENT_SUCCESS;
   locClientReqUnionType reqUnion;
@@ -1097,6 +1103,17 @@ bool locClientRegisterEventMask(
   memset(&regEventsReq, 0, sizeof(regEventsReq));
 
   regEventsReq.eventRegMask = eventRegMask;
+  regEventsReq.clientStrId_valid = true;
+  if (bIsMaster) {
+      LOC_LOGV("%s:%d] %s", __func__, __LINE__, MASTER_HAL);
+      strlcpy(regEventsReq.clientStrId, MASTER_HAL,
+              sizeof(regEventsReq.clientStrId));
+  }
+  else {
+      LOC_LOGV("%s:%d] %s", __func__, __LINE__, HAL);
+      strlcpy(regEventsReq.clientStrId, HAL,
+              sizeof(regEventsReq.clientStrId));
+  }
   reqUnion.pRegEventsReq = &regEventsReq;
 
   status = locClientSendReq(clientHandle,
@@ -1657,6 +1674,12 @@ static bool validateRequest(
         break;
     }
 
+    case QMI_LOC_REGISTER_MASTER_CLIENT_REQ_V02 :
+    {
+        *pOutLen = sizeof(qmiLocRegisterMasterClientReqMsgT_v02);
+        break;
+    }
+
     // ALL requests with no payload
     case QMI_LOC_GET_SERVICE_REVISION_REQ_V02:
     case QMI_LOC_GET_FIX_CRITERIA_REQ_V02:
@@ -1910,7 +1933,7 @@ locClientStatusEnumType locClientOpenInstance (
      // set the handle to the callback data
     *pLocClientHandle = (locClientHandleType)pCallbackData;
 
-    if(true != locClientRegisterEventMask(*pLocClientHandle,eventRegMask))
+    if (true != locClientRegisterEventMask(*pLocClientHandle, eventRegMask, false))
     {
       LOC_LOGE("%s:%d]: Error sending registration mask\n",
                   __func__, __LINE__);

sm6150-dev/android_device_xiaomi_sm6150-common

loc_api/loc_api_v02/loc_api_v02_client.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, 2018, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -678,6 +678,10 @@ static const locClientRespIndTableStructT locClientRespIndTable[]= {
    { QMI_LOC_SET_BLACKLIST_SV_IND_V02,
      sizeof(qmiLocGenReqStatusIndMsgT_v02) },
 
+    // register master
+   { QMI_LOC_REGISTER_MASTER_CLIENT_IND_V02,
+     sizeof(qmiLocRegisterMasterClientIndMsgT_v02) },
+
    { QMI_LOC_GET_BLACKLIST_SV_IND_V02,
      sizeof(qmiLocGetBlacklistSvIndMsgT_v02) },
 
@@ -1097,7 +1101,8 @@ static void locClientIndCb
 
 bool locClientRegisterEventMask(
     locClientHandleType clientHandle,
-    locClientEventMaskType eventRegMask)
+    locClientEventMaskType eventRegMask,
+    bool bIsMaster)
 {
   locClientStatusEnumType status = eLOC_CLIENT_SUCCESS;
   locClientReqUnionType reqUnion;
@@ -1106,6 +1111,17 @@ bool locClientRegisterEventMask(
   memset(&regEventsReq, 0, sizeof(regEventsReq));
 
   regEventsReq.eventRegMask = eventRegMask;
+  regEventsReq.clientStrId_valid = true;
+  if (bIsMaster) {
+      LOC_LOGV("%s:%d] %s", __func__, __LINE__, MASTER_HAL);
+      strlcpy(regEventsReq.clientStrId, MASTER_HAL,
+              sizeof(regEventsReq.clientStrId));
+  }
+  else {
+      LOC_LOGV("%s:%d] %s", __func__, __LINE__, HAL);
+      strlcpy(regEventsReq.clientStrId, HAL,
+              sizeof(regEventsReq.clientStrId));
+  }
   reqUnion.pRegEventsReq = &regEventsReq;
 
   status = locClientSendReq(clientHandle,
@@ -1666,6 +1682,12 @@ static bool validateRequest(
         break;
     }
 
+    case QMI_LOC_REGISTER_MASTER_CLIENT_REQ_V02 :
+    {
+        *pOutLen = sizeof(qmiLocRegisterMasterClientReqMsgT_v02);
+        break;
+    }
+
     // ALL requests with no payload
     case QMI_LOC_GET_SERVICE_REVISION_REQ_V02:
     case QMI_LOC_GET_FIX_CRITERIA_REQ_V02:
@@ -1919,7 +1941,7 @@ locClientStatusEnumType locClientOpenInstance (
      // set the handle to the callback data
     *pLocClientHandle = (locClientHandleType)pCallbackData;
 
-    if(true != locClientRegisterEventMask(*pLocClientHandle,eventRegMask))
+    if (true != locClientRegisterEventMask(*pLocClientHandle, eventRegMask, false))
     {
       LOC_LOGE("%s:%d]: Error sending registration mask\n",
                   __func__, __LINE__);
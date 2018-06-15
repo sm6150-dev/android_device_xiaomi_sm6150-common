9978c0409eaac6b3f450db2db7cf6c4bbd363189
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2018-06-15 14:26:50 +0530

    DPM :Remove tcmiface from PRODUCT_BOOT_JARS in vendor_bootjars
    Removed tcmiface from PRODUCT_BOOT_JARS in vendor_bootjars
    
    Change-Id: I587ea1d2dd6394e82e1996b9c902d188e8df26cc

--- build/tasks/vendor_bootjars.mk
+++ build/tasks/vendor_bootjars.mk
@@ -3,9 +3,6 @@
 
 ifneq ($(call is-vendor-board-platform,QCOM),true)
 
-#add extra jars here
-PRODUCT_BOOT_JARS += tcmiface
-
 #call dex_preopt.mk for extra jars
 include $(BUILD_SYSTEM)/dex_preopt.mk
 

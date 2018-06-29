6b8604ee17bb0631377f23033672b2ee22c02995
Katz Yamada <kyamada@codeaurora.org>
2018-06-29 14:00:29 -0700

    fix: Check basename when parsing clients
    Hal daemon to send ready ind message only to
    location clients by checking basename of client
    sockets.
    
    Change-Id: I6df406bf388d2f089453960c4e5d9c29d0b38989
    CRs-Fixed: 2270725

--- location_hal_daemon/LocationApiService.cpp
+++ location_hal_daemon/LocationApiService.cpp
@@ -151,6 +151,7 @@ void LocationApiService::onListenerReady() {
 
     struct dirent *dp = nullptr;
     struct stat sbuf = {0};
+    const std::string fnamebase = SOCKET_TO_LOCATION_CLIENT_BASE;
     while (nullptr != (dp = readdir(dirp))) {
         std::string fname = SOCKET_DIR_TO_CLIENT;
         fname += dp->d_name;
@@ -158,6 +159,9 @@ void LocationApiService::onListenerReady() {
             continue;
         }
         if ('.' == (dp->d_name[0])) {
+            continue;
+        }
+        if (0 != fname.compare(0, fnamebase.size(), fnamebase)) {
             continue;
         }
         if (S_ISSOCK((mode_t)sbuf.st_mode)) {

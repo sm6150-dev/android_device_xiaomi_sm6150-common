35333e907d99b03c3b2211ac900c8f1e266edbd6
Archana Sriram <apsrir@codeaurora.org>
2018-07-26 19:00:56 +0530

    Graphics: Set GPU frequencies from vendor init context
    Since ro.gpu.available_frequencies can only be set from
    vendor init context, modified to set this property from
    init.qcom.rc that runs in vendor_init context.
    
    Change-Id: I23107c18e67bd41989461b27889e6e2c93711d47
    CRs-Fixed: 2283789

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -447,8 +447,8 @@ else
     setprop ro.alarm_boot false
 fi
 
-# copy GPU frequencies to system property
+# copy GPU frequencies to vendor property
 if [ -f /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies ]; then
     gpu_freq=`cat /sys/class/kgsl/kgsl-3d0/gpu_available_frequencies` 2> /dev/null
-    setprop ro.gpu.available_frequencies "$gpu_freq"
+    setprop vendor.gpu.available_frequencies "$gpu_freq"
 fi

3f99ed5aacd3e404d70b55bc1b84fdd159ce470a
David Ng <dave@codeaurora.org>
2018-08-08 14:35:48 -0700

    Generate metadata.img if configured
    If board has metadataimage configuration, generate an empty
    ext4 metadata image.
    
    Also fix up unnecessary yaffs parameter in image size checking
    calls and add direct persistimage target to ease development.
    
    Change-Id: Ib2c50582f204e7bb6b0c0134af719ad6a39945a8

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -121,7 +121,7 @@ define build-persistimage-target
     @mkdir -p $(TARGET_OUT_PERSIST)
     $(hide) $(MKEXTUSERIMG) $(TARGET_OUT_PERSIST) $@ ext4 persist $(BOARD_PERSISTIMAGE_PARTITION_SIZE)
     $(hide) chmod a+r $@
-    $(hide) $(call assert-max-image-size,$@,$(BOARD_PERSISTIMAGE_PARTITION_SIZE),yaffs)
+    $(hide) $(call assert-max-image-size,$@,$(BOARD_PERSISTIMAGE_PARTITION_SIZE))
 endef
 
 $(INSTALLED_PERSISTIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS) $(INTERNAL_PERSISTIMAGE_FILES)
@@ -129,6 +129,38 @@ $(INSTALLED_PERSISTIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS) $(INTERNAL_PERS
 
 ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_PERSISTIMAGE_TARGET)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_PERSISTIMAGE_TARGET)
+
+.PHONY: persistimage
+persistimage: $(INSTALLED_PERSISTIMAGE_TARGET)
+
+endif
+
+#----------------------------------------------------------------------
+# Generate metadata image (metadata.img)
+# As of now this in empty at build and data is runtime generated only,
+# so create an empty fs
+#----------------------------------------------------------------------
+ifneq ($(strip $(BOARD_METADATAIMAGE_PARTITION_SIZE)),)
+
+TARGET_OUT_METADATA := $(PRODUCT_OUT)/metadata
+
+INSTALLED_METADATAIMAGE_TARGET := $(PRODUCT_OUT)/metadata.img
+
+define build-metadataimage-target
+    $(call pretty,"Target metadata fs image: $(INSTALLED_METADATAIMAGE_TARGET)")
+    @mkdir -p $(TARGET_OUT_METADATA)
+    $(hide) $(MKEXTUSERIMG) -s $(TARGET_OUT_METADATA) $@ ext4 metadata $(BOARD_METADATAIMAGE_PARTITION_SIZE)
+    $(hide) chmod a+r $@
+endef
+
+$(INSTALLED_METADATAIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS)
+	$(build-metadataimage-target)
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_METADATAIMAGE_TARGET)
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_METADATAIMAGE_TARGET)
+
+.PHONY: metadataimage
+metadataimage: $(INSTALLED_METADATAIMAGE_TARGET)
 
 endif
 
@@ -448,7 +480,7 @@ define build-small-userdataimage
   $(hide) mkdir -p $(1)
   $(hide) $(MKEXTUSERIMG) -s $(TARGET_OUT_DATA) $(2) ext4 data $(BOARD_SMALL_USERDATAIMAGE_PARTITION_SIZE)
   $(hide) chmod a+r $@
-  $(hide) $(call assert-max-image-size,$@,$(BOARD_SMALL_USERDATAIMAGE_PARTITION_SIZE),yaffs)
+  $(hide) $(call assert-max-image-size,$@,$(BOARD_SMALL_USERDATAIMAGE_PARTITION_SIZE))
 endef
 
 
@@ -539,4 +571,5 @@ otavendormod-nodeps:
 	$(board-vendorkernel-ota)
 
 $(BUILT_SYSTEMIMAGE): otavendormod
+
 endif

ee6ca3a398c871230409b264e97664baf6bdecbb
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 15:36:00 -0400

    sm6150-common: Create mount point symlinks for backward compatibility
    Change-Id: I54c244a41e2060a1f6b8d70cc4bd7c4ec0a39c10
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,6 +200,11 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /mnt/vendor/persist:/persist \
+    /vendor/bt_firmware:/bt_firmware \
+    /vendor/dsp:/dsp \
+    /vendor/firmware_mnt:/firmware
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true

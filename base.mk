e2529cb64b1bdef16b8e4b18a1a18caf1d7d2fd3
Urvashi Agrawal <urvaagra@codeaurora.org>
2018-09-06 12:00:38 -0700

    Update Vulkan hardware level supported to 1
    This change ensures that Vulkan hardware level
    is been updated to 1.
    
    Change-Id: Id3c811b47e51ca2ae81959a0f4c788947d713ed9
    Signed-off-by: Urvashi Agrawal <urvaagra@codeaurora.org>

--- base.mk
+++ base.mk
@@ -1081,7 +1081,7 @@ endif
 
 ifneq ($(TARGET_NOT_SUPPORT_VULKAN),true)
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml
 endif

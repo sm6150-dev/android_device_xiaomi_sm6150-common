c25ebc08f396b901791ba7ed03577d9274911061
zljing <zljing@codeaurora.org>
2018-09-11 14:38:52 +0800

    Revert "boot: shorten wait time for services exit to optimize shutdown time"
    Revert this change due to vendor no permission to
    set system property with non settable.
    This reverts commit 5836269d5f01292f652200c81e1cbcf55595d313.
    
    CRs-Fixed: 2312044
    Change-Id: Ibd34eb8b3255cddaa93d597b0e350bb612a21837

--- common.mk
+++ common.mk
@@ -29,8 +29,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.rat_on=combine \
     persist.backup.ntpServer=0.pool.ntp.org \
-    sys.vendor.shutdown.waittime=500 \
-    ro.build.shutdown_timeout=0
+    sys.vendor.shutdown.waittime=500
 
 ifneq ($(BOARD_FRP_PARTITION_NAME),)
     PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/$(BOARD_FRP_PARTITION_NAME)

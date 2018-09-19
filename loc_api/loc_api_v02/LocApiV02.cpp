08371dc1a0f34fac56970c60b607284c81a968d3
Mike Cailean <mcailean@codeaurora.org>
2018-09-19 16:17:25 -0700

    Lower severity of a log message
    When sending register master client QMI LOC message,
    if modem's version doesn't support this feature
    we will get an error message. Since this isn't
    actually an error use a warning log message instead
    of an error one
    
    Change-Id: I2c02539814226093a1c79d5d58b64a6834fa64c1
    CRs-fixed: 2318886

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -1331,7 +1331,7 @@ LocApiV02::registerMasterClient()
 
   if (eLOC_CLIENT_SUCCESS != status ||
       eQMI_LOC_REGISTER_MASTER_CLIENT_SUCCESS_V02 != reg_master_client_ind.status) {
-    LOC_LOGe ("error status = %s, reg_master_client_ind.status = %s",
+    LOC_LOGw ("error status = %s, reg_master_client_ind.status = %s",
               loc_get_v02_client_status_name(status),
               loc_get_v02_qmi_reg_mk_status_name(reg_master_client_ind.status));
     err = LOCATION_ERROR_GENERAL_FAILURE;

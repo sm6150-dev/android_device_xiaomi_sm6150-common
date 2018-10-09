CodeLinaro-mirror/femto5g_platform_vendor_qcom-opensource_location

FR48381: Support leap second change event
Support leap second change event
(1) Info will be available in location api and location
    client api
(2) Engine hub aggregator will use the info to calculate
    UTC timestamp

Change-Id: I8be18859bb5e2a4c47577ec6f39e5994fbb8158d
CRs-fixed: 2289457

loc_api/loc_api_v02/loc_api_v02_log.c
@@ -408,7 +408,10 @@ static const loc_name_val_s_type loc_v02_event_name[] =
     NAME_VAL(QMI_LOC_ENABLE_POSITION_ASSISTED_CLOCK_EST_IND_V02),
     NAME_VAL(QMI_LOC_QUERY_GNSS_ENERGY_CONSUMED_REQ_V02),
     NAME_VAL(QMI_LOC_QUERY_GNSS_ENERGY_CONSUMED_RESP_V02),
-    NAME_VAL(QMI_LOC_QUERY_GNSS_ENERGY_CONSUMED_IND_V02)
+    NAME_VAL(QMI_LOC_QUERY_GNSS_ENERGY_CONSUMED_IND_V02),
+    NAME_VAL(QMI_LOC_SYSTEM_INFO_REQ_V02),
+    NAME_VAL(QMI_LOC_SYSTEM_INFO_RESP_V02),
+    NAME_VAL(QMI_LOC_SYSTEM_INFO_IND_V02)
 };
 static const int loc_v02_event_num = sizeof(loc_v02_event_name) / sizeof(loc_name_val_s_type);
 

sm6150-dev/android_device_xiaomi_sm6150-common

loc_api/loc_api_v02/loc_api_v02_log.c
@@ -433,7 +433,10 @@ static const loc_name_val_s_type loc_v02_event_name[] =
     NAME_VAL(QMI_LOC_EVENT_GLONASS_EPHEMERIS_REPORT_IND_V02),
     NAME_VAL(QMI_LOC_EVENT_BDS_EPHEMERIS_REPORT_IND_V02),
     NAME_VAL(QMI_LOC_EVENT_GALILEO_EPHEMERIS_REPORT_IND_V02),
-    NAME_VAL(QMI_LOC_EVENT_QZSS_EPHEMERIS_REPORT_IND_V02)
+    NAME_VAL(QMI_LOC_EVENT_QZSS_EPHEMERIS_REPORT_IND_V02),
+    NAME_VAL(QMI_LOC_SYSTEM_INFO_REQ_V02),
+    NAME_VAL(QMI_LOC_SYSTEM_INFO_RESP_V02),
+    NAME_VAL(QMI_LOC_SYSTEM_INFO_IND_V02)
 };
 static const int loc_v02_event_num = sizeof(loc_v02_event_name) / sizeof(loc_name_val_s_type);
 
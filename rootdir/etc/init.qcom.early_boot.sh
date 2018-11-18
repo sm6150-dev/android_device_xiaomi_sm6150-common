a3a262406477d29262174c5c6885c9873cc497ee
Sandeep Panda <spanda@codeaurora.org>
2018-11-18 07:29:35 +0530

    init: early_boot: Properly parse mode width size from mode name
    Change-Id: I7af7e1f0e20835c1e0e382d1651ee5cf177395ae

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -50,7 +50,7 @@ if [ -f /sys/class/drm/card0-DSI-1/modes ]; then
     echo "detect" > /sys/class/drm/card0-DSI-1/status
     mode_file=/sys/class/drm/card0-DSI-1/modes
     while read line; do
-        fb_width=${line%x*};
+        fb_width=${line%%x*};
         break;
     done < $mode_file
 elif [ -f /sys/class/graphics/fb0/virtual_size ]; then

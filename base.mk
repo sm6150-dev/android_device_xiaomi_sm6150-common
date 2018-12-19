0362296cec645d123d9445699f2977bfb2e998e1
Ananth Raghavan Subramanian <sananth@codeaurora.org>
2018-12-19 11:05:37 -0800

    base.mk: Only define PowerHAL for legacy targets
    Updated PowerHAL definition has been moved to device specific makefiles.
    
    Change-Id: Iaa71c687a1dcde6826423a2af8410ddcdd063e21

--- base.mk
+++ base.mk
@@ -653,10 +653,12 @@ LIBQDUTILS := libqdutils
 LIBQDMETADATA := libqdMetaData
 
 #LIBPOWER
+ifneq ($(TARGET_USES_NON_LEGACY_POWERHAL), true)
 LIBPOWER := power.qcom
 #LIBPOWER -- Add HIDL Packages
 LIBPOWER += android.hardware.power@1.0-impl
 LIBPOWER += android.hardware.power@1.0-service
+endif
 
 #LLVM for RenderScript
 #use qcom LLVM

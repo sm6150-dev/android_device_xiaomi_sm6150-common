dbf235ee566c486a2f468b66ae75ed7bd62b32ae
Yingjie Wang <yingjiewang@codeaurora.org>
2019-03-07 18:08:06 +0800

    Add charger indication receiver in Gnss Hidl
    Register HEALTH Hal client in all Gnss Hidl impl
    to acquire charger indication.
    
    Change-Id: I3fcc00dd14a95e2375ab27a8c84c260300f9e90d
    CRs-fixed: 2411445

--- android/Android.mk
+++ android/Android.mk
@@ -1,9 +1,12 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 LOCAL_PATH := $(call my-dir)
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+include $(CLEAR_VARS)
+DIR_LIST := $(LOCAL_PATH)
+include $(LOCAL_PATH)/utils/Android.mk
 ifeq ($(GNSS_HIDL_VERSION),1.1)
-include $(LOCAL_PATH)/1.1/Android.mk
+include $(DIR_LIST)/1.1/Android.mk
 else
-include $(LOCAL_PATH)/1.0/Android.mk
+include $(DIR_LIST)/1.0/Android.mk
 endif
 
 endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE

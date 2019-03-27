aef05b2a09f0cc0bf97f3f2ccc61e5fb516209a1
Kevin Tang <zhikait@codeaurora.org>
2019-03-27 11:08:28 -0700

    Add params to support GNSS HIDL 2.0
    Add parameters to network status update
    to support changes for GNSS HIDL 2.0
    
    Change-Id: Icce994a549c2b2d0675292caf93266fa22ad589c
    CRs-Fixed: 2397433

--- location_hal_daemon/LocationApiService.cpp
+++ location_hal_daemon/LocationApiService.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2019, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -511,7 +511,8 @@ void LocationApiService::updateNetworkAvailability(bool availability) {
     GnssInterface* gnssInterface = getGnssInterface();
     if (gnssInterface) {
         gnssInterface->updateConnectionStatus(
-                availability, loc_core::NetworkInfoDataItemBase::TYPE_UNKNOWN);
+                availability, loc_core::NetworkInfoDataItemBase::TYPE_UNKNOWN,
+                false, NETWORK_HANDLE_UNKNOWN);
     }
 }
 

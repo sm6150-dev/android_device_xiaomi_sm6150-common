77e8716b98db030b984442393ce82648c4d3d89d
Mike Cailean <mcailean@codeaurora.org>
2019-04-08 11:09:44 -0700

    Streamline measurements processing
    Consolidate the translation from QMI LOC into one
    place in order to minimize traffic
    
    Change-Id: Ib44158288f9f942e3882f136eaf16e223b17919e
    CRs-fixed: 2430668

--- loc_api/loc_api_v02/LocApiV02.h
+++ loc_api/loc_api_v02/LocApiV02.h
@@ -91,7 +91,7 @@ private:
   uint32_t mCounter;
   uint32_t mMinInterval;
   std::vector<adrData>  mADRdata;
-  GnssSvMeasurementSet*  mSvMeasurementSet;
+  GnssMeasurements*  mGnssMeasurements;
   size_t mBatchSize, mDesiredBatchSize;
   size_t mTripBatchSize, mDesiredTripBatchSize;
 
@@ -121,7 +121,7 @@ private:
       uint8_t gloFrequency);
 
   /*convert GnssMeasurement type from QMI LOC to loc eng format*/
-  bool convertGnssMeasurements (GnssMeasurementsData& measurementData,
+  bool convertGnssMeasurements (
       const qmiLocEventGnssSvMeasInfoIndMsgT_v02& gnss_measurement_report_ptr,
       int index);
 
@@ -139,6 +139,9 @@ private:
 
   /* get sv used count from sv mask */
   static int getNumSvUsed (uint64_t svUsedIdsMask, int totalSvCntInOneConstellation);
+
+  void convertGnssMeasurementsHeader(const Gnss_LocSvSystemEnumType locSvSystemType,
+      const qmiLocEventGnssSvMeasInfoIndMsgT_v02& gnss_measurement_info);
 
   /*convert LocGnssClock type from QMI LOC to loc eng format*/
   int convertGnssClock (GnssMeasurementsClock& clock,
@@ -159,9 +162,6 @@ private:
   /* convert satellite report to loc eng format and  send the converted
      report to loc eng */
   void reportSv (const qmiLocEventGnssSvInfoIndMsgT_v02 *gnss_report_ptr);
-
-  void reportSvMeasurement (
-  const qmiLocEventGnssSvMeasInfoIndMsgT_v02 *gnss_raw_measurement_ptr);
 
   void  reportSvPolynomial (
   const qmiLocEventGnssSvPolyIndMsgT_v02 *gnss_sv_poly_ptr);

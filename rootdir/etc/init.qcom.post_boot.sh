eb30592e940236b5bf43a89aea60cefffdec3a22
Tharun Kumar Merugu <mtharu@codeaurora.org>
2019-04-18 17:07:28 +0530

    init.qcom.post_boot.sh: Enable cdspl3 governor for Kona
    CDSP subsytem uses rpmsg to send CPU L3 clock requests via cdspl3
    governor. Use cdspl3 as the governor for CDSP L3 devfreq node for
    serving CDSP L3 clock requests on Kona.
    
    Change-Id: I2a49e14ab15a5b2c4b2c4a8eba548702fb98cfd9

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -4068,10 +4068,10 @@ case "$target" in
 		echo 400 > $memlat/mem_latency/ratio_ceil
 	    done
 
-	    #Enable powersave governor for L3 cdsp nodes
+	    #Enable cdspl3 governor for L3 cdsp nodes
 	    for l3cdsp in $device/*qcom,devfreq-l3/*cdsp-l3-lat/devfreq/*cdsp-l3-lat
 	    do
-                echo "powersave" > $l3cdsp/governor
+                echo "cdspl3" > $l3cdsp/governor
 	    done
 
 	    #Enable mem_latency governor for LLCC and DDR scaling

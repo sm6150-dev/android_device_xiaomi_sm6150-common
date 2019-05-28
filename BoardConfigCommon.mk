46e9f84ee6c8500ee1d16de5d6eb9a63f566e73c
Felix <google@ix5.org>
2019-05-28 13:42:39 +0200

    sm6150-common: Makefile: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated
    
    Change-Id: Ia66f8462d7b7c546d8bd4d25c6fdfd22b1726841
    Signed-off-by: Felix <google@ix5.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,7 +197,7 @@ TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_M4DEFS += \
     debugfs_clk=vendor_debugfs_clk \
     debugfs_ipc=vendor_debugfs_ipc \

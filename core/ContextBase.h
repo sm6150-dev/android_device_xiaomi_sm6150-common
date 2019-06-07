225fd88e64b57a0c9dc3fa532895013324fc3301
Santoshkumar Zalake <szalake@codeaurora.org>
2019-06-07 13:55:19 +0530

    Add GNSS Deployment Support.
    The GNSS Deployment is used to configure
    GNSS Hardware using gps.conf file.
    
    CRs-Fixed: 2480150
    
    Change-Id: Ia09d3165de0c2f80f23bef8c0431095b25d16ea2

--- core/ContextBase.h
+++ core/ContextBase.h
@@ -71,6 +71,7 @@ typedef struct loc_gps_cfg_s
     double         CONSTRAINED_TIME_UNCERTAINTY_THRESHOLD;
     uint32_t       CONSTRAINED_TIME_UNCERTAINTY_ENERGY_BUDGET;
     uint32_t       POSITION_ASSISTED_CLOCK_ESTIMATOR_ENABLED;
+    uint32_t       GNSS_DEPLOYMENT;
 } loc_gps_cfg_s_type;
 
 /* NOTE: the implementaiton of the parser casts number

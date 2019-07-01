markakash/device_xiaomi_cepheus

Create resource overlay folder for Automotive
The folder is originated from Mobile common folders:
  1. device/qcom/common/device
  2. device/qcom/common/product

Change-Id: Id1aa87103c53095f2815ff66a3b944fb2fb85677

base.mk
@@ -1153,8 +1153,13 @@ endif
 ifneq ($(strip $(TARGET_USES_RRO)),true)
 # enable overlays to use our version of
 # source/resources etc.
+ifneq ($(strip $(TARGET_BOARD_AUTO)),true)
 DEVICE_PACKAGE_OVERLAYS += device/qcom/common/device/overlay
 PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/product/overlay
+else
+DEVICE_PACKAGE_OVERLAYS += device/qcom/common/auto/device/overlay
+PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/auto/product/overlay
+endif
 endif
 
 # Set up flags to determine the kernel version

sm6150-dev/android_device_xiaomi_sm6150-common

base.mk
@@ -1194,8 +1194,13 @@ endif
 ifneq ($(strip $(TARGET_USES_RRO)),true)
 # enable overlays to use our version of
 # source/resources etc.
+ifneq ($(strip $(TARGET_BOARD_AUTO)),true)
 DEVICE_PACKAGE_OVERLAYS += device/qcom/common/device/overlay
 PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/product/overlay
+else
+DEVICE_PACKAGE_OVERLAYS += device/qcom/common/automotive/device/overlay
+PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/automotive/product/overlay
+endif
 endif
 
 # Set up flags to determine the kernel version
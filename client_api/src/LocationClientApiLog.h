987277a0b27b408fa6a4abfc4c4eee7ae6da1295
Kevin Tang <zhikait@codeaurora.org>
2019-08-06 16:38:38 -0700

    change some of the comments that contain prohibited words
    Change-Id: Ia9b66c0cc49076eb6109e3693ace1da94a180859
    CRs-Fixed: 2504418

--- client_api/src/LocationClientApiLog.h
+++ client_api/src/LocationClientApiLog.h
@@ -473,8 +473,7 @@ typedef enum {
 
 typedef enum {
     /** This is the propagated/aggregated reports from all engines
-        running on the system (e.g.: DR/SPE/PPE) according to proprietary
-        algorithm. */
+        running on the system (e.g.: DR/SPE/PPE). */
     CLIENT_DIAG_LOC_OUTPUT_ENGINE_FUSED = 0,
     /** This fix is the unmodified fix from modem GNSS engine */
     CLIENT_DIAG_LOC_OUTPUT_ENGINE_SPE   = 1,
@@ -591,9 +590,8 @@ typedef PACKED struct PACKED_POST {
     /** location engine type. When the fix. when the type is set to
         LOC_ENGINE_SRC_FUSED, the fix is the propagated/aggregated
         reports from all engines running on the system (e.g.:
-        DR/SPE/PPE) based proprietary algorithm. To check which
-        location engine contributes to the fused output, check for
-        locOutputEngMask. */
+        DR/SPE/PPE). To check which location engine contributes to
+        the fused output, check for locOutputEngMask. */
     clientDiagLocOutputEngineType locOutputEngType;
     /** when loc output eng type is set to fused, this field
         indicates the set of engines contribute to the fix. */

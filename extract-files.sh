be6954095a19b455fabe3efb310b3c04c075e1a8
PIPIPIG233666 <2212848813@qq.com>
2019-08-07 13:08:14 -0400

    sm6150-common: Oops that shouldn't be there
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    Change-Id: Ic2894f5f44305819e1e279f66acd56ef6af9f6ce

--- extract-files.sh
+++ extract-files.sh
@@ -54,7 +54,6 @@ function blob_fixup() {
         vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so )
                 patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
         ;;
-        case "${1}" in
         lib/libwfdnative.so | lib64/libwfdnative.so)
                 sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
         ;;

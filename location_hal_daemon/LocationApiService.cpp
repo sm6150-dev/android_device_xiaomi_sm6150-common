CodeLinaro-mirror/femto5g_platform_vendor_qcom-opensource_location

Location client API: add API to receive concurrent SPE/PPE position
Add API to receive concurrent SPE/PPE position reports when there
are DRE is enabled in the system with other engines

Change-Id: I9b57acc44c0d4b3a25bb861651e252b6e3d5c17e
CRs-fixed: 2487580

location_hal_daemon/LocationApiService.cpp
@@ -144,7 +144,12 @@ LocationApiService::LocationApiService() :
         pClient->updateSubscription(
                 E_LOC_CB_GNSS_LOCATION_INFO_BIT | E_LOC_CB_GNSS_SV_BIT);
 
-        pClient->startTracking(0, gGnssSessionTbfMs);
+        LocationOptions locationOption;
+        locationOption.size = sizeof(locationOption);
+        locationOption.minInterval = gGnssSessionTbfMs;
+        locationOption.minDistance = 0;
+
+        pClient->startTracking(locationOption);
         pClient->mTracking = true;
         pClient->mPendingMessages.push(E_LOCAPI_START_TRACKING_MSG_ID);
     }
@@ -415,7 +420,7 @@ void LocationApiService::startTracking(LocAPIStartTrackingReqMsg *pMsg) {
         return;
     }
 
-    if (!pClient->startTracking(pMsg->distanceInMeters, pMsg->intervalInMs)) {
+    if (!pClient->startTracking(pMsg->locOptions)) {
         LOC_LOGe("Failed to start session");
         return;
     }
@@ -464,7 +469,7 @@ void LocationApiService::updateTrackingOptions(LocAPIUpdateTrackingOptionsReqMsg
 
     LocHalDaemonClientHandler* pClient = getClient(pMsg->mSocketName);
     if (pClient) {
-        pClient->updateTrackingOptions(pMsg->distanceInMeters, pMsg->intervalInMs);
+        pClient->updateTrackingOptions(pMsg->locOptions);
         pClient->mPendingMessages.push(E_LOCAPI_UPDATE_TRACKING_OPTIONS_MSG_ID);
     }
 

sm6150-dev/android_device_xiaomi_sm6150-common

location_hal_daemon/LocationApiService.cpp
@@ -159,7 +159,12 @@ LocationApiService::LocationApiService(uint32_t autostart, uint32_t sessiontbfms
         pClient->updateSubscription(
                 E_LOC_CB_GNSS_LOCATION_INFO_BIT | E_LOC_CB_GNSS_SV_BIT);
 
-        pClient->startTracking(0, sessiontbfms);
+        LocationOptions locationOption;
+        locationOption.size = sizeof(locationOption);
+        locationOption.minInterval = sessiontbfms;
+        locationOption.minDistance = 0;
+
+        pClient->startTracking(locationOption);
         pClient->mTracking = true;
         pClient->mPendingMessages.push(E_LOCAPI_START_TRACKING_MSG_ID);
     }
@@ -440,7 +445,7 @@ void LocationApiService::startTracking(LocAPIStartTrackingReqMsg *pMsg) {
         return;
     }
 
-    if (!pClient->startTracking(pMsg->distanceInMeters, pMsg->intervalInMs)) {
+    if (!pClient->startTracking(pMsg->locOptions)) {
         LOC_LOGe("Failed to start session");
         return;
     }
@@ -489,7 +494,7 @@ void LocationApiService::updateTrackingOptions(LocAPIUpdateTrackingOptionsReqMsg
 
     LocHalDaemonClientHandler* pClient = getClient(pMsg->mSocketName);
     if (pClient) {
-        pClient->updateTrackingOptions(pMsg->distanceInMeters, pMsg->intervalInMs);
+        pClient->updateTrackingOptions(pMsg->locOptions);
         pClient->mPendingMessages.push(E_LOCAPI_UPDATE_TRACKING_OPTIONS_MSG_ID);
     }
 
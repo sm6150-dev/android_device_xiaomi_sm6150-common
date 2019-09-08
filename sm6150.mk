992faa7a5da79ead82193646a7604cb7eedb49d7
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-08 08:15:08 -0400

    sm6150-common: Build display stack from source [1/2]
     * Use graphic blobs from LA.UM.7.9.r1-07800-sm6150.0.
    
    Change-Id: Ic708ca0e5e32f835c74c37182c0e6dfdd7a1eab9

--- sm6150.mk
+++ sm6150.mk
@@ -143,14 +143,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.composer@2.2-impl \
+    android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    gralloc.sm6150 \
+    hwcomposer.sm6150 \
     libdisplayconfig \
-    libqdMetaData.system \
     libtinyxml \
     libvulkan \
-    vendor.display.config@1.7 \
-    vendor.display.config@1.7.vendor
+    memtrack.sm6150 \
+    vendor.qti.hardware.display.allocator@1.0-service
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -375,5 +377,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # WiFi Display
+PRODUCT_PACKAGES += \
+    libqdMetaData.system
+
 PRODUCT_BOOT_JARS += \
     WfdCommon

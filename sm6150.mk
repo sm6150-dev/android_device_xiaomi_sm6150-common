e86c9eb21632dd8349a8e8a5dfafbbf0eb6aaa2c
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 19:06:44 +0200

    sm6150-common: Remove PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE
    Change-Id: I2751014af71f222bd43e4bc17fc75d20499ea2f1

--- sm6150.mk
+++ sm6150.mk
@@ -24,9 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
-# Properties
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi

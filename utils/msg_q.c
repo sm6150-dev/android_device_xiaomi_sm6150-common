b3060216e0e03e19ec873f3bdb7cf2a0fae33d4b
haohuang <haohuang@codeaurora.org>
2019-09-16 16:03:09 +0800

    Logging buffer implemented by skip list
    Use skip list to implement a log buffer,
    log sentences will be inserted into this buffer
    once the log print function is called. the
    buffer will be dumped when GNSS process
    crash/killed or send SIGUSR1 signal manually.
    
    Change-Id: I5e23cdf30c263fa59108070c9dbfd95c49c2e9b6
    CRs-Fixed: 2571326

--- utils/msg_q.c
+++ utils/msg_q.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2012, 2014, 2017 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2012, 2014, 2017, 2020 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -26,6 +26,8 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// Uncomment to log verbose logs
+#define LOG_NDEBUG 1
 #define LOG_TAG "LocSvc_utils_q"
 #include <stdio.h>
 #include <stdlib.h>

9b88fae898d04cb8a4b58de8411bdf2163c5cc7b
Sandeep Neerudu <sneerudu@codeaurora.org>
2019-09-30 12:23:34 +0530

    sensors : remove sensors specific boot up commands from init.qcom.rc
    move sensors specific boot up commands from global init.qcom.rc to component
    
    specific rc files , which are part of individual component projects.
    
    Change-Id: Ib0410cd0af8d77573bd320063982b504b1b1d1d5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -271,13 +271,6 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE       := init.qcom.sensors.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.sensors.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
     include $(CLEAR_VARS)
     LOCAL_MODULE       := init.qcom.test.rc

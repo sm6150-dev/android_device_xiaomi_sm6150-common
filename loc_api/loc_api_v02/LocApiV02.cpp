RMX1801/android_device_realme_RMX1801

Location hal daemon: send power state change to modem
Send power state change to modem via Gnss Adapter interface

Change-Id: I399a4259a18c4d71b61f35c23adce19feaba6941
CRs-fixed: 2527622

loc_api/loc_api_v02/LocApiV02.cpp
@@ -6233,6 +6233,55 @@ LocationError LocApiV02::getGnssEnergyConsumed() {
     return err;
 }
 
+void LocApiV02 :: updatePowerState(PowerStateType powerState){
+    sendMsg(new LocApiMsg([this, powerState] () {
+
+    LocationError err = LOCATION_ERROR_SUCCESS;
+    qmiLocInjectPlatformPowerStateReqMsgT_v02 req;
+    qmiLocInjectPlatformPowerStateIndMsgT_v02 ind;
+
+    locClientStatusEnumType status;
+    locClientReqUnionType req_union;
+
+    LOC_LOGd("Enter. power state %d", powerState);
+    qmiLocPlatformPowerStateEnumT_v02 qmiPowerState = eQMI_LOC_POWER_STATE_UNKNOWN_V02;
+    switch (powerState) {
+    case POWER_STATE_SUSPEND:
+        qmiPowerState = eQMI_LOC_POWER_STATE_SUSPENDED_V02;
+        break;
+    case POWER_STATE_RESUME:
+        qmiPowerState = eQMI_LOC_POWER_STATE_RESUME_V02;
+        break;
+    case POWER_STATE_SHUTDOWN:
+        qmiPowerState = eQMI_LOC_POWER_STATE_SHUTDOWN_V02;
+        break;
+    default:
+        break;
+    }
+
+    // unknown power state will not be injected to modem
+    if (eQMI_LOC_POWER_STATE_UNKNOWN_V02 != qmiPowerState) {
+        memset(&req, 0, sizeof(req));
+        memset(&ind, 0, sizeof(ind));
+        req.powerState = qmiPowerState;
+        req_union.pInjectPowerStateReq = &req;
+
+        status = locSyncSendReq(QMI_LOC_INJECT_PLATFORM_POWER_STATE_REQ_V02,
+                                req_union, LOC_ENGINE_SYNC_REQUEST_TIMEOUT,
+                                QMI_LOC_INJECT_PLATFORM_POWER_STATE_IND_V02,
+                                &ind);
+        if(status != eLOC_CLIENT_SUCCESS || ind.status != eQMI_LOC_SUCCESS_V02) {
+            LOC_LOGe("failed. status: %s, ind status:%s\n",
+                     loc_get_v02_client_status_name(status),
+                     loc_get_v02_qmi_status_name(ind.status));
+            err = LOCATION_ERROR_GENERAL_FAILURE;
+        }
+    }
+
+    LOC_LOGd("Exit. err: %u", err);
+    }));
+}
+
 bool LocApiV02 :: cacheGnssMeasurementSupport()
 {
     bool gnssMeasurementSupported = false;

loc_api/loc_api_v02/LocApiV02.h
@@ -329,6 +329,7 @@ public:
   virtual LocationError setConstrainedTuncMode(bool enabled, float tuncConstraint, uint32_t powerBudget);
   virtual LocationError setPositionAssistedClockEstimatorMode(bool enabled);
   virtual LocationError getGnssEnergyConsumed();
+  virtual void updatePowerState(PowerStateType powerState);
   virtual void requestForAidingData(GnssAidingDataSvMask svDataMask);
 
   /*

location_hal_daemon/LocationApiService.cpp
@@ -847,6 +847,15 @@ void LocationApiService::onShutdown() {
     onSuspend();
     LOC_LOGd("--< onShutdown");
 }
+
+void LocationApiService::injectPowerEvent(PowerStateType powerState) {
+    GnssInterface* gnssInterface = getGnssInterface();
+    if (!gnssInterface) {
+        LOC_LOGe(">-- getGnssEnergyConsumed null GnssInterface");
+        return;
+    }
+    gnssInterface->updatePowerState(powerState);
+}
 #endif
 
 /******************************************************************************
@@ -872,22 +881,9 @@ GnssInterface* LocationApiService::getGnssInterface() {
     static GnssInterface* gnssInterface = nullptr;
 
     if (nullptr == gnssInterface && !getGnssInterfaceFailed) {
-        LOC_LOGd("Loading libgnss.so::getGnssInterface ...");
-        getLocationInterface* getter = NULL;
-        const char *error = NULL;
-        dlerror();
-        void *handle = dlopen("libgnss.so", RTLD_NOW);
-        if (nullptr == handle) {
-            LOC_LOGe("dlopen for libgnss.so failed");
-        } else if (nullptr != (error = dlerror()))  {
-            LOC_LOGe("dlopen for libgnss.so failed, error = %s", error);
-        } else {
-            getter = (getLocationInterface*)dlsym(handle, "getGnssInterface");
-            if ((error = dlerror()) != NULL)  {
-                LOC_LOGe("dlsym for getGnssInterface failed, error = %s", error);
-                getter = NULL;
-            }
-        }
+        void * tempPtr = nullptr;
+        getLocationInterface* getter = (getLocationInterface*)
+                dlGetSymFromLib(tempPtr, "libgnss.so", "getGnssInterface");
 
         if (nullptr == getter) {
             getGnssInterfaceFailed = true;

sm6150-dev/android_device_xiaomi_sm6150-common

loc_api/loc_api_v02/LocApiV02.cpp
@@ -6636,6 +6636,55 @@ LocationError LocApiV02::getGnssEnergyConsumed() {
     return err;
 }
 
+void LocApiV02 :: updateSystemPowerState(PowerStateType powerState){
+    sendMsg(new LocApiMsg([this, powerState] () {
+
+    LocationError err = LOCATION_ERROR_SUCCESS;
+    qmiLocInjectPlatformPowerStateReqMsgT_v02 req;
+    qmiLocInjectPlatformPowerStateIndMsgT_v02 ind;
+
+    locClientStatusEnumType status;
+    locClientReqUnionType req_union;
+
+    LOC_LOGd("Enter. power state %d", powerState);
+    qmiLocPlatformPowerStateEnumT_v02 qmiPowerState = eQMI_LOC_POWER_STATE_UNKNOWN_V02;
+    switch (powerState) {
+    case POWER_STATE_SUSPEND:
+        qmiPowerState = eQMI_LOC_POWER_STATE_SUSPENDED_V02;
+        break;
+    case POWER_STATE_RESUME:
+        qmiPowerState = eQMI_LOC_POWER_STATE_RESUME_V02;
+        break;
+    case POWER_STATE_SHUTDOWN:
+        qmiPowerState = eQMI_LOC_POWER_STATE_SHUTDOWN_V02;
+        break;
+    default:
+        break;
+    }
+
+    // unknown power state will not be injected to modem
+    if (eQMI_LOC_POWER_STATE_UNKNOWN_V02 != qmiPowerState) {
+        memset(&req, 0, sizeof(req));
+        memset(&ind, 0, sizeof(ind));
+        req.powerState = qmiPowerState;
+        req_union.pInjectPowerStateReq = &req;
+
+        status = locSyncSendReq(QMI_LOC_INJECT_PLATFORM_POWER_STATE_REQ_V02,
+                                req_union, LOC_ENGINE_SYNC_REQUEST_TIMEOUT,
+                                QMI_LOC_INJECT_PLATFORM_POWER_STATE_IND_V02,
+                                &ind);
+        if (status != eLOC_CLIENT_SUCCESS || ind.status != eQMI_LOC_SUCCESS_V02) {
+            LOC_LOGe("failed. status: %s, ind status:%s\n",
+                     loc_get_v02_client_status_name(status),
+                     loc_get_v02_qmi_status_name(ind.status));
+            err = LOCATION_ERROR_GENERAL_FAILURE;
+        }
+    }
+
+    LOC_LOGd("Exit. err: %u", err);
+    }));
+}
+
 bool LocApiV02 :: cacheGnssMeasurementSupport()
 {
     bool gnssMeasurementSupported = false;

loc_api/loc_api_v02/LocApiV02.h
@@ -407,6 +407,7 @@ public:
   virtual LocationError setConstrainedTuncMode(bool enabled, float tuncConstraint, uint32_t powerBudget);
   virtual LocationError setPositionAssistedClockEstimatorMode(bool enabled);
   virtual LocationError getGnssEnergyConsumed();
+  virtual void updateSystemPowerState(PowerStateType powerState);
   virtual void requestForAidingData(GnssAidingDataSvMask svDataMask);
 
   /*

location_hal_daemon/LocationApiService.cpp
@@ -842,6 +842,15 @@ void LocationApiService::onShutdown() {
     onSuspend();
     LOC_LOGd("--< onShutdown");
 }
+
+void LocationApiService::injectPowerEvent(PowerStateType powerState) {
+    GnssInterface* gnssInterface = getGnssInterface();
+    if (!gnssInterface) {
+        LOC_LOGe(">-- getGnssEnergyConsumed null GnssInterface");
+        return;
+    }
+    gnssInterface->updateSystemPowerState(powerState);
+}
 #endif
 
 /******************************************************************************
@@ -867,22 +876,9 @@ GnssInterface* LocationApiService::getGnssInterface() {
     static GnssInterface* gnssInterface = nullptr;
 
     if (nullptr == gnssInterface && !getGnssInterfaceFailed) {
-        LOC_LOGd("Loading libgnss.so::getGnssInterface ...");
-        getLocationInterface* getter = NULL;
-        const char *error = NULL;
-        dlerror();
-        void *handle = dlopen("libgnss.so", RTLD_NOW);
-        if (nullptr == handle) {
-            LOC_LOGe("dlopen for libgnss.so failed");
-        } else if (nullptr != (error = dlerror()))  {
-            LOC_LOGe("dlopen for libgnss.so failed, error = %s", error);
-        } else {
-            getter = (getLocationInterface*)dlsym(handle, "getGnssInterface");
-            if ((error = dlerror()) != NULL)  {
-                LOC_LOGe("dlsym for getGnssInterface failed, error = %s", error);
-                getter = NULL;
-            }
-        }
+        void * tempPtr = nullptr;
+        getLocationInterface* getter = (getLocationInterface*)
+                dlGetSymFromLib(tempPtr, "libgnss.so", "getGnssInterface");
 
         if (nullptr == getter) {
             getGnssInterfaceFailed = true;
fdc8d8e4cbb1656958a7339f0ddbea2e10bd5e0d
Justin Yun <justinyun@google.com>
2019-12-21 10:25:24 +0900

    sm6150-common: Enforce native interfaces of product partition as VNDK
    By setting PRODUCT_PRODUCT_VNDK_VERSION := current, native modules in
    product partition are not allowed to use any libs in system partition
    excepted VNDK.
    Enforcing it for coral devices.
    
    Bug: 145580164
    Test: Build and check boot
    Change-Id: Icb818c3c6e74a1211881671d87d205d949209b92

--- sm6150.mk
+++ sm6150.mk
@@ -374,6 +374,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+# VNDK
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \

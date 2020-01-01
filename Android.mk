3813db8a480c9a06887e0e246426a086da9a942f
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-31 20:39:22 -0500

    sm6150-common: Add support for phoenix and tucana
    Change-Id: Id926ff5d3070b8799ecf78b9d48593cac04b7ab2

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter davinci violet,$(TARGET_DEVICE)),)
+ifneq ($(filter davinci phoenix tucana violet,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,7 +26,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "davinci violet"
+write_headers "davinci phoenix tucana violet"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

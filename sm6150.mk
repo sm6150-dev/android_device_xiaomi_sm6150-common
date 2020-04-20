21a43a03052042551023be8ffa187613c07edcbb
Pig <pig.priv@gmail.com>
2020-04-20 06:06:47 +0200

    sm6150-common: Build 32-bit audio impl only
    Change-Id: I61dc8c41dc35a637be3dc59c31e96784eacc3dd7

--- sm6150.mk
+++ sm6150.mk
@@ -86,10 +86,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@5.0-impl:32 \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.audio@5.0-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.sm6150 \
     audio.r_submix.default \

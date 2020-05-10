f6b7890e57ea26ddeec3944803c9196ffae128fd
Pig <pig.priv@gmail.com>
2020-05-10 02:15:18 +0200

    sm6150-common: fod: Make sure HBM is disabled on hide
     * There are some cases which on delay is bigger than the off
     one hence it can't be disabled on relase.
    
    Change-Id: I5979f1ff842f506cacfebde89d58a03afe48070a

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -108,6 +108,7 @@ Return<void> FingerprintInscreen::onShowFODView() {
 
 Return<void> FingerprintInscreen::onHideFODView() {
     TouchFeatureService->setTouchMode(10, 0);
+    xiaomiDisplayFeatureService->setFeature(0, 11, 0, 3);
     xiaomiDisplayFeatureService->setFeature(0, 17, 0, 255);
     return Void();
 }

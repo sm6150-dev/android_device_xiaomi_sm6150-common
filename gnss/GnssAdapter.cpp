c17260c02e25563a07156941f1bb7ae38a3388f5
Wei (GPS SD) Chen <weic@codeaurora.org>
2020-05-29 16:00:20 -0700

    GPS HAL: allow client to start tracking session when registering for none-position related callbacks
    When locaiton client has callback for sv, meas, nmea, gps debug
    data, the location client should be considered as GNSS client
    and allow session to be started
    
    Change-Id: I7641c3eaff64cfd076ee678530fb33610a30fe76
    CRs-fixed: 2698513

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -2639,7 +2639,8 @@ GnssAdapter::hasCallbacksToStartTracking(LocationAPI* client)
     auto it = mClientData.find(client);
     if (it != mClientData.end()) {
         if (it->second.trackingCb || it->second.gnssLocationInfoCb ||
-            it->second.engineLocationsInfoCb || it->second.gnssMeasurementsCb) {
+                it->second.engineLocationsInfoCb || it->second.gnssMeasurementsCb ||
+                it->second.gnssDataCb || it->second.gnssSvCb || it->second.gnssNmeaCb) {
             allowed = true;
         } else {
             LOC_LOGi("missing right callback to start tracking")

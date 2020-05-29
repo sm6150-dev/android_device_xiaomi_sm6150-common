112fb0d2dead3f06aa1a44d37f7a0d970e319870
Mike Cailean <mcailean@codeaurora.org>
2020-05-29 09:29:19 -0700

    Jammer Indicator is not sent to LCA
    CRs-fixed: 2698638
    
    Change-Id: I46d3a996989e2c48507bbe6e473008b987805cf5

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -3324,11 +3324,12 @@ GnssAdapter::reportPositionEvent(const UlpLocation& ulpLocation,
     // this position is from QMI LOC API, then send report to engine hub
     // also, send out SPE fix promptly to the clients that have registered
     // with SPE report
-    LOC_LOGd("reportPositionEvent, eng type: %d, unpro %d, sess status %d",
-             locationExtended.locOutputEngType, ulpLocation.unpropagatedPosition, status);
+    LOC_LOGd("reportPositionEvent, eng type: %d, unpro %d, sess status %d msInWeek %d",
+             locationExtended.locOutputEngType,
+             ulpLocation.unpropagatedPosition, status, msInWeek);
 
     if (false == ulpLocation.unpropagatedPosition && pDataNotify != nullptr) {
-        reportDataEvent((GnssDataNotification&)pDataNotify, msInWeek);
+        reportDataEvent((GnssDataNotification&)(*pDataNotify), msInWeek);
     }
 
     if (true == initEngHubProxy()){
@@ -3853,7 +3854,7 @@ GnssAdapter::reportDataEvent(const GnssDataNotification& dataNotify,
             mMsInWeek(msInWeek) {
         }
         inline virtual void proc() const {
-            if (-1 != mMsInWeek) {
+            if (mMsInWeek >= 0) {
                 mAdapter.getDataInformation((GnssDataNotification&)mDataNotify,
                                             mMsInWeek);
             }

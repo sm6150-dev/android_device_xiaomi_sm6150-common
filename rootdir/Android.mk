8b1e93b87f3208ddef62f6f27950fc290c1a0a5b
merothh <merothh@gmail.com>
2020-06-05 22:39:43 +0000

    sm6150-common: introduce fstab_dynamic.qcom
    Change-Id: Iddda6d133c0326103c1bae18c808a24fbd13e608

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -72,8 +72,20 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
+LOCAL_SRC_FILES    := etc/fstab_dynamic.qcom
+else
 LOCAL_SRC_FILES    := etc/fstab.qcom
+endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.ramdisk
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab_dynamic.qcom
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

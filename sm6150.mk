68b762311b2b51bf94490ce915a36d6abc121354
Nitin Shivpure <nshivpur@codeaurora.org>
2020-06-15 10:48:05 +0530

    sm6150-common: bt: Add vendor.qti.hardware.btconfigstore@2.0 into product package
    Add vendor.qti.hardware.btconfigstore@2.0 into product package.
    
    CRs-fixed: 2739162
    Change-Id: I36ca083bf333826ac8980541181aa52022671396

--- sm6150.mk
+++ sm6150.mk
@@ -142,7 +142,8 @@ PRODUCT_PACKAGES += \
     BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \

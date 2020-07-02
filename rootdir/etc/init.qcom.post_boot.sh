f4082248beaf35519e10264fa0af3bee96df6742
Santosh Mardi <gsantosh@codeaurora.org>
2020-07-02 07:32:26 +0530

    post_boot: memlat settings moved to different target specific files
    update post boot for lagoon to remove memlat settings and move to
    new script in different target specific folder and set property
    which triggers new scripts for memlat.
    
    Change-Id: Ifd7d9cce4061fba849d3a1e4064c3106e90b6f6a

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -3741,31 +3741,10 @@ case "$target" in
                 echo 250 > $npullccbw/bw_hwmon/up_scale
                 echo 0 > /sys/devices/virtual/npu/msm_npu/pwr
             done
-
-            #Enable mem_latency governor for L3, LLCC, and DDR scaling
-            for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
-            do
-                echo "mem_latency" > $memlat/governor
-                echo 10 > $memlat/polling_interval
-                echo 400 > $memlat/mem_latency/ratio_ceil
-            done
-
-            #Enable cdspl3 governor for L3 cdsp nodes
-            for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
-            do
-                echo "cdspl3" > $l3cdsp/governor
-            done
-
-            #Gold L3 ratio ceil
-            echo 4000 > /sys/class/devfreq/soc:qcom,cpu6-cpu-l3-lat/mem_latency/ratio_ceil
-
-            #Enable compute governor for gold latfloor
-            for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
-            do
-                echo "compute" > $latfloor/governor
-                echo 10 > $latfloor/polling_interval
-            done
         done
+        # memlat specific settings are moved to seperate file under
+        # device/target specific folder
+        setprop vendor.dcvs.prop 1
 
         # cpuset parameters
         echo 0-5 > /dev/cpuset/background/cpus

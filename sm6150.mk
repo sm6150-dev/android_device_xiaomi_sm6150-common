41af7f868fba86d1545e667ef8f92484ccfd603d
Pig <pig.priv@gmail.com>
2020-07-19 18:18:25 +0200

    sm6150-common: Move libperfmgr to common
    Change-Id: I5aec1b3d96fc208a88d14ced079175332f4f7770

--- sm6150.mk
+++ sm6150.mk
@@ -300,8 +300,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.sm6150-libperfmgr \
-    powerhint.json
+    android.hardware.power@1.3-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf

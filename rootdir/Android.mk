386baa4bde2bf979510534cbad1df1b964ef48c8
Prerna Kalla <prernak@codeaurora.org>
2020-08-17 15:15:32 +0530

    sm6150-common: steppe: Support ufs and emmc variant fstab files
    Two fstabs are needed to support different data unit
    number (dun) size for ufs and emmc hardwares. This
    needs different encryption flag in fstab to address
    the difference in dun size.
    
    Change-Id: Ifb0e9132fc91e3281401ac49ad70c695a22c9df5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -71,30 +71,30 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE       := fstab.default
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 ifeq ($(PRODUCT_USE_DYNAMIC_PARTITIONS), true)
-LOCAL_SRC_FILES    := etc/fstab_dynamic.qcom
+LOCAL_SRC_FILES    := etc/default/fstab_non_AB_dynamic_partition.qti
 else
-LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_SRC_FILES    := etc/default/fstab.qcom
 endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.ramdisk
-LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE       := fstab.emmc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab_dynamic.qcom
-LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+LOCAL_SRC_FILES    := etc/emmc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.persist
+LOCAL_MODULE       := fstab.ramdisk
+LOCAL_MODULE_STEM  := fstab.default
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.persist
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_SRC_FILES    := etc/default/fstab_non_AB_dynamic_partition.qti
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)

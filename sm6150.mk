f2cf65eab5ae3ae8edddb55086372be5691273d5
Prerna Kalla <prernak@codeaurora.org>
2020-08-17 15:15:32 +0530

    sm6150-common: steppe: Support ufs and emmc variant fstab files
    Two fstabs are needed to support different data unit
    number (dun) size for ufs and emmc hardwares. This
    needs different encryption flag in fstab to address
    the difference in dun size.
    
    Change-Id: Ifb0e9132fc91e3281401ac49ad70c695a22c9df5

--- sm6150.mk
+++ sm6150.mk
@@ -298,8 +298,8 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    fstab.persist \
-    fstab.qcom \
+    fstab.default \
+    fstab.emmc \
     init.insmod.sh \
     init.mi.usb.sh \
     init.power.rc \

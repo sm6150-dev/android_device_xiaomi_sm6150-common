3cee69b59f0fc5c4707274b3691ffc4dfe2e9d99
PIPIPIG233666 <pig.priv@gmail.com>
2020-09-08 16:24:58 -0400

    sm6150-common: Import libperfmgr/pixel-power-ext from hardware/google
    Change-Id: Id53c9ce373f80d4f1ab97f61da67aaf70e1563d5

--- sm6150.mk
+++ sm6150.mk
@@ -329,6 +329,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Dependencies 

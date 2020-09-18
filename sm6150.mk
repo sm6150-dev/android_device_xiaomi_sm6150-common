65f85f23e9886b3b1e1a72547f2adee372cd9279
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    sm6150-common: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- sm6150.mk
+++ sm6150.mk
@@ -375,12 +375,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sm6150
-
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2

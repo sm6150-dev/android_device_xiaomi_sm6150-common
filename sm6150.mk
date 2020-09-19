1aa0219a3bda4d6a0858edb330dbfab97726b61f
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    sm6150-common: Import QCOM bluetooth soong namespace
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84

--- sm6150.mk
+++ sm6150.mk
@@ -354,7 +354,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \

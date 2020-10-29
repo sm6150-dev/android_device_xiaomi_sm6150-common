4dd4a0080ec712a49a39e911cdc6edf99d25b53b
LuK1337 <priv.luk@gmail.com>
2020-10-29 13:22:27 +0100

    sm6150-common: Enable RRO overlays for lineage-sdk and Snap
    This reverts commit a3cb3abae2 and 704bc23.
    
    Change-Id: I12f150b13686f8b91bb63e4ee728eb4016856aa5

--- sm6150.mk
+++ sm6150.mk
@@ -23,9 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true

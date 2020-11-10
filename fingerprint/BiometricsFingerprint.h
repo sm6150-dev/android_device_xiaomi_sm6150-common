9b38598d6eaedfdcad76a263e3556d2f4246873c
Pig <pig.priv@gmail.com>
2020-11-10 21:40:26 +0800

    sm6150-common: fingerprint: Opt-in for devices without fod
     * 11 enforces VINTF manifest registration, add fragment for devices
     with fod only and re-run clang-format.
     * Set SOONG_CONFIG_xiaomiVars_uses_fod_extension to true in BoardConfig
     if your device has fod.
    
    Change-Id: I016d707490e22cd215b2a49b2564acf9cb239e46

--- fingerprint/BiometricsFingerprint.h
+++ fingerprint/BiometricsFingerprint.h
@@ -35,7 +35,9 @@
 
 #include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
 
+#ifdef USES_FOD_EXTENSION
 #include <vendor/xiaomi/hardware/fingerprintextension/1.0/IXiaomiFingerprint.h>
+#endif
 
 namespace android {
 namespace hardware {
@@ -44,19 +46,26 @@ namespace fingerprint {
 namespace V2_1 {
 namespace implementation {
 
+using ::android::sp;
+using ::android::status_t;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
 using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
 using ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprintClientCallback;
 using ::android::hardware::biometrics::fingerprint::V2_1::RequestStatus;
-using ::android::hardware::Return;
-using ::android::hardware::Void;
-using ::android::hardware::hidl_vec;
-using ::android::hardware::hidl_string;
-using ::android::sp;
-using ::android::status_t;
 
+#ifdef USES_FOD_EXTENSION
 using ::vendor::xiaomi::hardware::fingerprintextension::V1_0::IXiaomiFingerprint;
+#endif
 
+#ifdef USES_FOD_EXTENSION
 struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFingerprint {
+#else
+struct BiometricsFingerprint : public IBiometricsFingerprint {
+  public:
+#endif
     BiometricsFingerprint();
     ~BiometricsFingerprint();
 
@@ -65,10 +74,13 @@ struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFing
     // Method to wrap legacy HAL with BiometricsFingerprint class
     static IBiometricsFingerprint* getInstance();
 
-    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint follow.
-    Return<uint64_t> setNotify(const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
+    // Methods from ::android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint
+    // follow.
+    Return<uint64_t> setNotify(
+            const sp<IBiometricsFingerprintClientCallback>& clientCallback) override;
     Return<uint64_t> preEnroll() override;
-    Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid, uint32_t timeoutSec) override;
+    Return<RequestStatus> enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid,
+                                 uint32_t timeoutSec) override;
     Return<RequestStatus> postEnroll() override;
     Return<uint64_t> getAuthenticatorId() override;
     Return<RequestStatus> cancel() override;
@@ -77,10 +89,13 @@ struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFing
     Return<RequestStatus> setActiveGroup(uint32_t gid, const hidl_string& storePath) override;
     Return<RequestStatus> authenticate(uint64_t operationId, uint32_t gid) override;
 
+#ifdef USES_FOD_EXTENSION
     Return<int32_t> extCmd(int32_t cmd, int32_t param) override;
+#endif
 
     static fingerprint_device_t* openHal();
-    static void notify(const fingerprint_msg_t *msg); /* Static callback for legacy HAL implementation */
+    static void notify(
+            const fingerprint_msg_t* msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);
     static FingerprintAcquiredInfo VendorAcquiredFilter(int32_t error, int32_t* vendorCode);
@@ -88,7 +103,7 @@ struct BiometricsFingerprint : public IBiometricsFingerprint, public IXiaomiFing
 
     std::mutex mClientCallbackMutex;
     sp<IBiometricsFingerprintClientCallback> mClientCallback;
-    fingerprint_device_t *mDevice;
+    fingerprint_device_t* mDevice;
 };
 
 }  // namespace implementation

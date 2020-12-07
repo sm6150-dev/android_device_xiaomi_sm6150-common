3db0467b4761dd00d91cbab6939d9cbb7a4522cc
Pig <pig.priv@gmail.com>
2020-12-07 10:18:09 +0800

    sm6150-common: Move DPM feature property to /system_ext
     * DPM blobs are under /system_ext.
    
    Change-Id: I6bafdc3b310168c6b4d681c9eb94eb4e2e43c5e5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,6 +173,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # QCOM

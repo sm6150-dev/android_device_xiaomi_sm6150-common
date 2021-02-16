d66f29bc54231fa3d56b32d11963edc104123e0a
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sm6150-common: Migrate to Xiaomi power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- sm6150.mk
+++ sm6150.mk
@@ -286,8 +286,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.sm6150-libperfmgr \
-    powerhint.json
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
@@ -334,6 +333,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Dependencies 

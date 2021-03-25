46ca8dd7d70e22464fe2b53a646f684db70d81df
Edwin Wong <edwinwong@google.com>
2021-03-25 00:19:58 -0700

    sdm845-common: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I3adf55199301768bacb38103c3ef4ba24807df2f

--- sm6150.mk
+++ sm6150.mk
@@ -177,7 +177,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint feature
 PRODUCT_COPY_FILES += \

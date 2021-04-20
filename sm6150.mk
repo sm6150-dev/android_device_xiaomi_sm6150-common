6daa17fc39eb7729b169ef74f8658d8b0bb8f09c
Bruno Martins <bgcngm@gmail.com>
2021-04-20 21:56:48 +0100

    sm6150-common: Build power-off charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Change-Id: Ie013d25d55105b4eb2191b9fd625ed8e02c863c5

--- sm6150.mk
+++ sm6150.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # CNE
 PRODUCT_PACKAGES += \
     cneapiclient \

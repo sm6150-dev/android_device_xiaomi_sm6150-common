aaecf256c114534ff1c8e7ff0490e7ce6158544d
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:23:48 +0000

    raphael: Change method used for popup
    android s reports onCameraUnavailable during shutdown which causes
    camera to popup during shutdown or reboot hence switch to proper
    methods

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -71,8 +71,8 @@ public class PopupCameraService extends Service implements Handler.Callback {
     private CameraManager.AvailabilityCallback availabilityCallback =
             new CameraManager.AvailabilityCallback() {
                 @Override
-                public void onCameraAvailable(@NonNull String cameraId) {
-                    super.onCameraAvailable(cameraId);
+                public void onCameraClosed(@NonNull String cameraId) {
+                    super.onCameraClosed(cameraId);
                     if (cameraId.equals(Constants.FRONT_CAMERA_ID)) {
                         mClosedEvent = SystemClock.elapsedRealtime();
                         if (SystemClock.elapsedRealtime() - mOpenEvent <
@@ -86,8 +86,8 @@ public class PopupCameraService extends Service implements Handler.Callback {
                 }
 
                 @Override
-                public void onCameraUnavailable(@NonNull String cameraId) {
-                    super.onCameraAvailable(cameraId);
+                public void onCameraOpened(@NonNull String cameraId, @NonNull String packageId) {
+                    super.onCameraOpened(cameraId, packageId);
                     if (cameraId.equals(Constants.FRONT_CAMERA_ID)) {
                         mOpenEvent = SystemClock.elapsedRealtime();
                         if (SystemClock.elapsedRealtime() - mClosedEvent <

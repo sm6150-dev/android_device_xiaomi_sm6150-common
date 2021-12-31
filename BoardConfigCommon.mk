LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
extends system sepolicy. To that end, we use another make variable to
specify product sepolicy.

Bug: 119305624
Test: m selinux_policy
Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

BoardConfigCommon.mk
@@ -130,9 +130,9 @@ VENDOR_SECURITY_PATCH := 2020-12-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true

sm6150-dev/android_device_xiaomi_sm6150-common

BoardConfigCommon.mk
@@ -198,8 +198,6 @@ VENDOR_SECURITY_PATCH := 2021-02-01
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_M4DEFS += \
     debugfs_clk=vendor_debugfs_clk \
@@ -219,6 +217,8 @@ BOARD_SEPOLICY_M4DEFS += \
     sysfs_ssr=vendor_sysfs_ssr \
     wcnss_service_exec=vendor_wcnss_service_exec \
     wifi_vendor_data_file=vendor_wifi_vendor_data_file
+PRODUCT_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+PRODUCT_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 
 # Soong
 SOONG_CONFIG_NAMESPACES += xiaomiVars